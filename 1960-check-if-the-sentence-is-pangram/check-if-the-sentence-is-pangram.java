f4fe3806caceb4432d26d0145b30207769804b62
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-02-27 05:00:24 -0700

    Time: 3 ms (52.43%) | Memory: 41.8 MB (22.88%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        int[] alphabet = new int[26];
+        sentence=sentence.toLowerCase();
+        char[] ch = sentence.toCharArray();
+        int n = ch.length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(ch[i]>='a'&&ch[i]<='z'){
+                if(alphabet[ch[i]-'a']==0){
+                    count++;
+                    alphabet[ch[i]-'a']++;
+                }
+            }
+            if(count==26) return true;
+        }
+        /*StringBuilder sb = new StringBuilder();
+        for(int i=0;i<26;i++){
+            if(alphabet[i]==0){
+                char c = (char)('a'+i);
+                sb.append(c);
+            }
+        }
+        System.out.println(sb.toString());*/
+        //if(count==26) return true;
+        return false;
+    }
+}
