ac483810295166d3eac4e60c57cb331d98e078f1
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-02-29 23:54:43 -0700

    Time: 3 ms (97.97%) | Memory: 59.7 MB (61.74%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] corpFlightBookings(int[][] b, int n) {
-         int[] ans = new int[n];
+        int[] ans = new int[n];
         for(int i=0;i<b.length;i++){
-            // ans[b[i][0]-1] += b[i][2];
-            // ans[b[i][1]-1] += b[i][2];
-            for(int j=b[i][0];j<=b[i][1];j++){
-                ans[j-1] += b[i][2];
-            }
+            ans[b[i][1]-1]+=b[i][2];
+            if(b[i][0]-2>=0)ans[b[i][0]-2]-=b[i][2];
+        }
+        for(int i=n-2;i>=0;i--){
+            ans[i]+=ans[i+1];
         }
         return ans;
     }
