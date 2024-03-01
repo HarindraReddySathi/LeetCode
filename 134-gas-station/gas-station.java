678e874bacfe131e834d3d63d9ba4bf8f0e40678
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-01 10:29:47 -0700

    Time: 542 ms (5.01%) | Memory: 63 MB (5.48%) - LeetSync

---
+++ 134-gas-station/gas-station.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+        int[] memo = new int[2*n];
+        //int zc=0;
+        for(int i=0;i<n;i++){
+            if(gas[i]!=0 || cost[i]!=0){
+                 System.out.println(i+"---"+gas[i]+"--"+cost[i]);
+            }
+            int k = gas[i]-cost[i];
+            memo[i]=k;
+            memo[n+i]=k;
+        }
+        //System.out.println(zc+"---"+n);
+        int i=0;
+        int j=0;
+        int sum=0;
+        while(i<n){
+            //System.out.println(i+"---"+j+"--"+sum);
+            if(memo[i]<0){
+                if(sum!=0)sum-=memo[i];
+                i++;
+                j=Math.max(i,j);
+                continue;
+            }
+            sum+=memo[j];
+            j++;
+            while(sum<0){
+                sum-=memo[i];
+                i++;
+            }
+            if(sum>=0 && j-i==n) return i;
+        }
+        return -1;
+    }
+}
