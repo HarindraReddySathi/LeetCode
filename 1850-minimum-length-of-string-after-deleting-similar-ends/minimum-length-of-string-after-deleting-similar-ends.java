3016572a900839b5c2f7cbf18ec4096435fcc12b
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-02 02:28:09 -0700

    Time: 3 ms (100.00%) | Memory: 45 MB (74.24%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
@@ -7,11 +7,9 @@ class Solution {
         int j=n-1;
         while(j>i){
             if(ch[i]!=ch[j]) break;
-            if(j-i==2) return 1;
-            i++;j--;
-            while(j>i && ch[i]==ch[i-1])i++;
-            if(i==j) return 0;
-            while(j>i && ch[j]==ch[j+1])j--;
+            char c = ch[i];
+            while(j>=i && ch[i]==c)i++;
+            while(j>=i && ch[j]==c)j--;
         }
         return j-i+1;
     }
