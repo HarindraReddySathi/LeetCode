6d0288aec919ce0704116bd52a7d9432937b3538
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-03 13:58:03 -0700

    Time: 1 ms (99.04%) | Memory: 40.4 MB (72.12%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
@@ -1,48 +1,48 @@
 class Solution {
     public int maxDiff(int num) {
         
-        char[] ch = String.valueOf(num).toCharArray();
-        int n = ch.length;
-        //To get Maximum
-        char val ='$';
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(ch[i]!='9' && val =='$'){
-                val = ch[i];
-                ch[i]='9';
-                count++;
-                continue;
-            }
-            if(ch[i]==val){
-                ch[i]='9';
-                continue;
-            }
-        }
-        int max = Integer.parseInt(String.valueOf(ch));
-        System.out.println(max);
-        // To get MIN
-        ch = String.valueOf(num).toCharArray();
-        val ='$';
-        char change ='$';
-        count=0;
-        for(int i=0;i<n;i++){
-            if((val =='$')&& ((i!=0&&ch[i]!='0') || (i==0 && ch[i]!='1'))){
-                if(!(ch[i]=='1' && ch[0]=='1')){
-                    val = ch[i];
-                    ch[i]=(i==0)?'1':'0';
-                    change = (i==0)?'1':'0';
-                    count++;
-                }
-                continue;
-            }
-            if(ch[i]==val){
-                ch[i]=change;
-                continue;
-            }
-        }
-        int min = Integer.parseInt(String.valueOf(ch));
-        System.out.println(min);
+        char[] nums = Integer.toString(num).toCharArray();
 
-        return max-min;
+        char[] a = nums.clone();
+        char[] b = nums.clone();
+        char x = '*';
+        char y = '*';
+        char max = '9', min = '1';
+
+        // maximum character
+        if (nums[0] != '9')
+            x = nums[0];
+        else {
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] != '9') {
+                    x = nums[i];
+                    break;
+                }
+            }
+        }
+
+        // minimum character
+        if (nums[0] != '1') {
+            min = '1';
+            y = nums[0];
+        } else {
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] != '1' && nums[i] != '0') {
+                    min = '0';
+                    y = nums[i];
+                    break;
+                }
+            }
+        }
+
+        // Replacement
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == x)
+                a[i] = max;
+            if (nums[i] == y)
+                b[i] = min;
+        }
+        return Integer.parseInt(String.valueOf(a)) -
+                Integer.parseInt(String.valueOf(b));
     }
 }
