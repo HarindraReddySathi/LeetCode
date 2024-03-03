9eb1572dc5ab04a060353c2a55bbe58afcae6809
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-03 13:55:24 -0700

    Time: 5 ms (24.04%) | Memory: 40.5 MB (58.65%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
@@ -10,18 +10,14 @@ class Solution {
             if(ch[i]!='9' && val =='$'){
                 val = ch[i];
                 ch[i]='9';
-                //System.out.println(ch[i]);
                 count++;
                 continue;
             }
             if(ch[i]==val){
                 ch[i]='9';
-                //System.out.println(ch[i]);
                 continue;
             }
-            //System.out.println(ch[i]);
         }
-        //if(count==0)Arrays.fill(ch,'8');
         int max = Integer.parseInt(String.valueOf(ch));
         System.out.println(max);
         // To get MIN
@@ -31,23 +27,18 @@ class Solution {
         count=0;
         for(int i=0;i<n;i++){
             if((val =='$')&& ((i!=0&&ch[i]!='0') || (i==0 && ch[i]!='1'))){
-                System.out.println(ch[i]+"----"+ch[0]);
-                System.out.println(!(ch[i]==1 && ch[0]==1));
                 if(!(ch[i]=='1' && ch[0]=='1')){
                     val = ch[i];
                     ch[i]=(i==0)?'1':'0';
                     change = (i==0)?'1':'0';
-                    System.out.println(ch[i]);
                     count++;
                 }
                 continue;
             }
             if(ch[i]==val){
                 ch[i]=change;
-                //System.out.println(ch[i]);
                 continue;
             }
-            //System.out.println(ch[i]);
         }
         int min = Integer.parseInt(String.valueOf(ch));
         System.out.println(min);
