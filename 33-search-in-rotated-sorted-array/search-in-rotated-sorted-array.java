b1e5ecabbd3089aa30463b9efef3e2280413191b
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-04 00:50:20 -0700

    Time: 2 ms (5.53%) | Memory: 42.3 MB (13.75%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -23,20 +23,6 @@ class Solution {
         }
         return l;
     }
-    public int decisionMaker(int[] nums, int mid){
-        if(mid==0){
-            if(nums[mid]>nums[mid+1]) return 0;
-            return 1;
-        }else if(mid == nums.length-1){
-            if(nums[mid]>nums[mid-1]) return 0;
-            return 2;
-        }else{
-            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]) return 0;
-            else if(nums[mid]>nums[nums.length-1] && nums[mid]>nums[0]) return 1;
-            else return 2;
-        }
-    }
-
     public int binarySearch(int[]nums,int low,int high,int target){
         if(high<low) return -1;
         int mid = low+(high-low)/2;
