a4b7aaab911b59a8f48bd87d02a3c7011736ee82
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-04 00:12:22 -0700

    Time: 7 ms (97.35%) | Memory: 42.4 MB (74.34%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        
+        int n = s.length();
+        char[] ch = s.toCharArray();
+        int[] ans = new int[2];
+        int max =1;
+        int[] temp = new int[2];
+        for(int i=1;i<n;i++){
+            maxPalindrome(ch,i,n,temp);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=temp[0];i<=temp[1];i++)sb.append(ch[i]);
+        return sb.toString();
+    }
+
+    public void maxPalindrome(char[] ch, int i ,int n,int[] temp){
+        caseOorE(ch,i,i,n,temp);
+        caseOorE(ch,i-1,i,n,temp);
+    }
+
+    public void caseOorE(char[] ch,int left,int right,int n,int[] temp){
+        int ans =0;
+        int dec = (left==right)?-1:0;
+        
+        while(left>=0 && right<n){
+            //System.out.println(left+"---"+right);
+            if(ch[left]==ch[right]) ans++;
+            else break;
+            left--;right++;
+        }
+        ans*=2;ans+=dec;
+        if((temp[1]-temp[0]+1)<ans){
+            //System.out.println(left+"---"+right+"****");
+            temp[0]=left+1;
+            temp[1]=right-1;
+            //System.out.println(temp[0]+"---"+temp[1]);
+        }
+    }
+}
