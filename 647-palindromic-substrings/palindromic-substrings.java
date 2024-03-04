db19ddbeab9fc318682d6ca155dc2d9125cd7509
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-03 23:30:11 -0700

    Time: 2548 ms (5.01%) | Memory: 45 MB (27.37%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countSubstrings(String s) {
+        
+        int n = s.length();
+        char[] ch = s.toCharArray();
+        int ans =1;
+        for(int i=1;i<n;i++){
+            ans+=findPalindromeCouunt(ch,i,n);
+        }
+        return ans;
+    }
+
+    public int findPalindromeCouunt(char[] ch ,int i,int n){
+        int ans =0;
+        ans+=Case(ch,i,i,n);
+        ans+=Case(ch,i-1,i,n);
+        return ans;
+    }
+    public int Case(char[] ch, int left,int right,int n){
+        int ans =0;
+        while(left>=0 && right<n){
+            System.out.println(left+"---"+right);
+            if(ch[left--]==ch[right++]) ans++;
+            else break;
+        }
+        return ans;
+    }
+}
