def3e48f61232f5ed7daf760ed5a1ce90d0c3910
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-04 19:45:27 -0700

    Time: 2 ms (20.67%) | Memory: 42.2 MB (14.09%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -4,38 +4,27 @@ class Solution {
         int len = s.length();
         char[] ch = s.toCharArray();
         if(ch[0]=='0')return 0;
+        if(len==1) return 1;
         Set<String> set = new HashSet<>();
         set.add("1");set.add("2");set.add("3");set.add("4");set.add("5");set.add("6");
         set.add("7");set.add("8");set.add("9");set.add("10");set.add("11");set.add("12");
         set.add("13");set.add("14");set.add("15");set.add("16");set.add("17");set.add("18");
         set.add("19");set.add("20");set.add("21");set.add("22");set.add("23");set.add("24");
         set.add("25");set.add("26");
+        StringBuilder sb = new StringBuilder();
         int[] ans = new int[len];
-        for(int i=len-1;i>=0;i--){
-            StringBuilder sb = new StringBuilder();
-            sb.append(ch[i]);
-            if(i==len-1){
-                if(set.contains(sb.toString())){
-                    ans[i]=1;
-                }
-            }else if (i==len-2){
-                if(set.contains(sb.toString())){
-                    ans[i]+=ans[i+1];
-                }
-                sb.append(ch[i+1]);
-                if(set.contains(sb.toString())){
-                    ans[i]+=1;
-                }
-            }else{
-                if(set.contains(sb.toString())){
-                    ans[i]+=ans[i+1];
-                }
-                sb.append(ch[i+1]);
-                if(set.contains(sb.toString())){
-                    ans[i]+=ans[i+2];
-                }
-            }
+        ans[0]=1;
+        if(ch[1]!='0') ans[1]++;
+        sb.append(ch[0]);
+        sb.append(ch[1]);
+        if(set.contains(sb.toString()))ans[1]++;
+        for(int i=2;i<len;i++){
+            sb = new StringBuilder();
+            if(ch[i]!='0')ans[i]+=ans[i-1];
+            sb.append(ch[i-1]);sb.append(ch[i]);
+            if(set.contains(sb.toString()))ans[i]+=ans[i-2];
+            if(ans[i]==0) return 0;
         }
-        return ans[0];
+        return ans[len-1];
     }
 }
