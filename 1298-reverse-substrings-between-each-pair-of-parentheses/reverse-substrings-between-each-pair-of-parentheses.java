7b656548eb2ee9c1aeeeb7c4b66bf8fb4eadfd23
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-05 21:29:47 -0700

    Time: 2 ms (71.22%) | Memory: 41.9 MB (56.29%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -8,7 +8,6 @@ class Solution {
         for(int i=0;i<n;i++){
             if(ch[i]=='('){
                 if(sb.length()!=0)s.push(sb.toString());
-                if(sb.length()!=0) System.out.println(sb.toString());
                 s.push("(");
                 sb = new StringBuilder();
             }else if(ch[i]!=')') sb.append(ch[i]);
@@ -18,9 +17,7 @@ class Solution {
                 while(!s.isEmpty() && !s.peek().equals("(")){
                     temp=s.pop()+temp;
                 }
-                System.out.println(temp);
                 sb = new StringBuilder(temp);
-                //s.push(temp);
             }
         }
         return sb.toString();
