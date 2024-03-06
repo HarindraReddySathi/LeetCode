1b094b534f28b8bca9ba81ca83210bd8146631dd
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-05 23:14:24 -0700

    Time: 7 ms (97.28%) | Memory: 42.1 MB (87.34%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -2,36 +2,39 @@ class Solution {
     public String longestPalindrome(String s) {
         
         int n = s.length();
-        char[] ch = s.toCharArray();
-        int[] ans = new int[2];
-        int max =1;
-        int[] temp = new int[2];
+        char[] ch= s.toCharArray();
+
+        int[] bound = new int[2];
+        int max = 1;
+
+        //oddCase
+        for(int i=0;i<n;i++){
+            int j=i;
+            int k=i;
+            while(j>=0 && k<n && ch[j]==ch[k]){
+                int len = k-j+1;
+                if(len>max){
+                    max=len;bound[0]=j;bound[1]=k;
+                }
+                 j--;k++;
+            }
+        }
+
+        //evenCase
         for(int i=1;i<n;i++){
-            maxPalindrome(ch,i,n,temp);
+            int j=i-1;
+            int k=i;
+            while(j>=0 && k<n && ch[j]==ch[k]){
+                int len = k-j+1;
+                if(len>max){
+                    max=len;bound[0]=j;bound[1]=k;
+                }
+                j--;k++;
+            }
         }
+
         StringBuilder sb = new StringBuilder();
-        for(int i=temp[0];i<=temp[1];i++)sb.append(ch[i]);
+        for(int i=bound[0];i<=bound[1];i++)sb.append(ch[i]);
         return sb.toString();
-    }
-
-    public void maxPalindrome(char[] ch, int i ,int n,int[] temp){
-        caseOorE(ch,i,i,n,temp);
-        caseOorE(ch,i-1,i,n,temp);
-    }
-
-    public void caseOorE(char[] ch,int left,int right,int n,int[] temp){
-        int ans =0;
-        int dec = (left==right)?-1:0;
-        
-        while(left>=0 && right<n){
-            if(ch[left]==ch[right]) ans++;
-            else break;
-            left--;right++;
-        }
-        ans*=2;ans+=dec;
-        if((temp[1]-temp[0]+1)<ans){
-            temp[0]=left+1;
-            temp[1]=right-1;
-        }
     }
 }
