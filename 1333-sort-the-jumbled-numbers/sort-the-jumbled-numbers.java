09b6b48fbba41d35a1af28c0dd531a14eec2c209
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-06 01:47:18 -0700

    Time: 130 ms (65.43%) | Memory: 55.6 MB (53.09%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0;i<n;i++){
             ans.add(new Pair(nums[i],converted(mapping,nums[i]),i));
         }
-        Collections.sort(ans,(a, b) -> a.compare(b));
+        Collections.sort(ans);
         for(int i=0;i<n;i++){
             nums[i]=ans.get(i).encrypted;
         }
@@ -13,6 +13,35 @@ class Solution {
     }
 
     public int converted(int[] mapping,int n){
+        char[] ch = String.valueOf(n).toCharArray();
+        for(int i=0;i<ch.length;i++){
+            int k = ch[i]-'0';
+            ch[i]=(char)(mapping[k]+'0');
+        }
+        return Integer.parseInt(String.valueOf(ch));
+    }
+
+    class Pair implements Comparable<Pair>{
+        int encrypted;
+        int actual;
+        int position;
+        public Pair(int encrypted,int actual,int position){
+            this.encrypted=encrypted;
+            this.actual=actual;
+            this.position = position;
+        }
+        
+        @Override
+        public int compareTo(Pair other){
+            if (this.actual != other.actual) {
+                return this.actual>other.actual?1:-1;
+            } else {
+                return this.position>other.position?1:-1;
+            }
+        }
+    }
+    
+    /*public int converted(int[] mapping,int n){
         Stack<Integer> s = new Stack<>();
         if(n==0) return mapping[0];
         while(n>0){
@@ -24,23 +53,5 @@ class Solution {
             ans+=ans*10+s.pop();
         }
         return ans;
-    }
-
-    class Pair{
-        int encrypted;
-        int actual;
-        int position;
-        public Pair(int encrypted,int actual,int position){
-            this.encrypted=encrypted;
-            this.actual=actual;
-            this.position = position;
-        }
-        public int compare(Pair other){
-            if (this.actual != other.actual) {
-                return Integer.compare(this.actual, other.actual);
-            } else {
-                return Integer.compare(this.position, other.position);
-            }
-        }
-    }
+    }*/
 }
