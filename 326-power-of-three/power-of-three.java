d3af2e1eb20d7cd230facf673d385debd05711a0
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-06 15:29:42 -0700

    Time: 26 ms (5.75%) | Memory: 44.8 MB (8.72%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -4,6 +4,7 @@ class Solution {
         if(n==1) return true;
         if(n%3!=0) return false;
         return isPowerOfThree(n/3);*/
-        return n > 0 && 1162261467 % n == 0;
+        //return n > 0 && 1162261467 % n == 0;
+        return Integer.toString(n, 3).matches("^10*$");
     }
 }
