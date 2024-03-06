8e54adb9fefd3e4f422f229a000a00fa5ee8cdac
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-05 21:39:58 -0700

    Time: 0 ms (100.00%) | Memory: 42.7 MB (85.53%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,46 +1,35 @@
 class Solution {
-
     public void nextPermutation(int[] nums) {
-        int n= nums.length;
-        if(n==1) return;
-        if(n==2){
-            swap(nums,0,1);
-            return;
-        }
-        //Finding the first decreasing element
-        int first =-1;
+        
+        int n =nums.length;
+        int dip=-1;
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
-                 first =i;
-                 break;
+                dip=i;
+                break;
             }
         }
-
-        if(first!=-1){
-            for(int i=n-1;i>first;i--){
-                if(nums[i]>nums[first]){
-                    swap(nums,i,first);
+        if(dip!=-1){
+            for(int i=n-1;i>dip;i--){
+                if(nums[i]>nums[dip]){
+                    swap(nums,i,dip);
                     break;
                 }
             }
         }
-        reverse(nums,first+1);
+        reverse(nums,dip+1,n-1);
     }
 
-    public void reverse(int[] nums,int i){
-        int n = nums.length;
-        for(int j=0;j<(n-i+1)/2;j++){
-            int temp = nums[i+j];
-            nums[i+j]=nums[n-j-1];
-            nums[n-j-1]=temp;
+    public void reverse(int[] nums,int start,int end){
+        while(start<end){
+            swap(nums,start,end);
+            start++;end--;
         }
     }
 
-    public void swap(int[] nums,int i,int j){
-        System.out.println(i+"--"+j);
-        int t= nums[i];
+    public void swap(int[] nums,int i ,int j){
+        int temp = nums[i];
         nums[i]=nums[j];
-        nums[j]=t;
+        nums[j]=temp;
     }
-    
 }
