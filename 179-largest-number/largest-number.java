67bcd3ebfb6c036bc78ddde18a943740e607c080
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-06 13:56:56 -0700

    Time: 5 ms (96.86%) | Memory: 43.1 MB (60.11%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -1,31 +1,27 @@
 class Solution {
     public String largestNumber(int[] nums) {
+
         int n = nums.length;
         String[] s = new String[n];
-        for (int i = 0; i < n; i++) {
-            s[i] = Integer.toString(nums[i]);
+        for(int i =0;i<n;i++)s[i]=String.valueOf(nums[i]);
+        int count=0;
+        for(int i =0;i<n;i++){
+            if(nums[i]==0) count++;
         }
-
-        // Custom comparator
-        Arrays.sort(s, new Comparator<String>() {
-            @Override
-            public int compare(String a, String b) {
-                String ab = a + b;
-                String ba = b + a;
-                return ba.compareTo(ab); // Compare concatenated strings
+        if(count==n) return "0";
+        Comparator<String> comp =new Comparator<>(){
+            public int compare(String n1,String n2){
+                String s1 = n1+n2;
+                String s2 = n2+n1;
+                return s2.compareTo(s1);
             }
-        });
+        };
+        Arrays.sort(s,comp);
 
         StringBuilder sb = new StringBuilder();
-        for (String str : s) {
-            sb.append(str);
+        for(int i=0;i<n;i++){
+            sb.append(s[i]);
         }
-        if(sb.charAt(0)=='0') return "0";
         return sb.toString();
     }
-    /*
-        write a custom comparator for sorting the string array defined above such that 
-        when comparing two strings we need to check integer value of s[i]+s[j] is greater than s[j+s[i]]
-
-    */
 }
