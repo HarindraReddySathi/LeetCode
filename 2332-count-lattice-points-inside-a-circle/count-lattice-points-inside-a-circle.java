a94e5bb3a0bb00af4da2429d35b1aa7c4eb5dd64
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-09 12:24:14 -0700

    Time: 23 ms (94.29%) | Memory: 41.2 MB (88.57%) - LeetSync

--- 2332-count-lattice-points-inside-a-circle/count-lattice-points-inside-a-circle.java
+++ 2332-count-lattice-points-inside-a-circle/count-lattice-points-inside-a-circle.java
@@ -1,8 +1,17 @@
 class Solution {
     public int countLatticePoints(int[][] circles) {
         int ans =0;
-        for(int i =0;i<201;i++){
-            for(int j =0;j<201;j++){
+        int maxX = 0;
+        int maxY = 0;
+        for(int k = 0;k<circles.length;k++){
+            int r = circles[k][2];
+            int x = (circles[k][0]+r);
+            int y = (circles[k][1]+r);
+            if(x>maxX) maxX=x;
+            if(y>maxY) maxY=y;
+        }
+        for(int i =0;i<=maxX;i++){
+            for(int j =0;j<=maxY;j++){
                 for(int k = 0;k<circles.length;k++){
                     int r = circles[k][2]*circles[k][2];
                     int x = (circles[k][0]-i)*(circles[k][0]-i);
