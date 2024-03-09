e86ab46649074e4486dc5075c572c5941b8c12ba
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-09 13:32:19 -0700

    Time: 362 ms (30.30%) | Memory: 70.2 MB (61.62%) - LeetSync

--- 2333-count-number-of-rectangles-containing-each-point/count-number-of-rectangles-containing-each-point.java
+++ 2333-count-number-of-rectangles-containing-each-point/count-number-of-rectangles-containing-each-point.java
@@ -5,23 +5,15 @@ class Solution {
         Arrays.sort(rectangles, Comparator.comparingInt(rectangle -> rectangle[0]));
         Map<Integer,List<Integer>> map = new HashMap<>();
         for(int[] i : rectangles){
-            //System.out.println(i[0]+"----"+i[1]);
             map.computeIfAbsent(i[1],l->new ArrayList<>()).add(i[0]);
-        }
-        for(Integer i : map.keySet()){
-            //System.out.println(i +"------"+map.get(i).size());
-            //for(int j : map.get(i))System.out.print(j+"--");
-            //System.out.println();
         }
 
         int count =0;
         for(int[] i : points){
             int x = i[0];int y = i[1];
             int temp=0;
-            //System.out.println("****######****");
             for(int k : map.keySet()){
                 if(k<y) continue;
-                //System.out.println("######");
                 List<Integer> xPoints = map.get(k);
                 int low=0;int high = xPoints.size()-1;
                 int cur =-1;
