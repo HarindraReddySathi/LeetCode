15c182ef266eb39131b8a099b13410aba72ac051
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-11 03:13:43 -0700

    Time: 6 ms (76.97%) | Memory: 44.1 MB (87.58%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -4,8 +4,11 @@ class Solution {
         Map<Integer,Integer> map = new HashMap<>();
         for(int i :nums){
             map.put(i,map.getOrDefault(i,0)+1);
+            //freq.computeIfAbsent(map.get(i),l->new TreeSet<>(Collections.reverseOrder())).add(i);
+            //if(map.get(i)-1!=0)freq.get(map.get(i)-1).remove(i);
+        }
+        for(Integer i : map.keySet()){
             freq.computeIfAbsent(map.get(i),l->new TreeSet<>(Collections.reverseOrder())).add(i);
-            if(map.get(i)-1!=0)freq.get(map.get(i)-1).remove(i);
         }
         int pos =0;
         for(Integer i : freq.keySet()){
