739036f86f0305b1bdad071b4c19c5ccf574b017
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-11 01:52:41 -0700

    Time: 3 ms (89.09%) | Memory: 42.6 MB (90.73%) - LeetSync

--- 1044-find-common-characters/find-common-characters.java
+++ 1044-find-common-characters/find-common-characters.java
@@ -1,29 +1,28 @@
 class Solution {
     public List<String> commonChars(String[] words) {
         
-        Map<Character,Integer> main = new HashMap<>();
+        int[] main = new int[26];
         
         for(int i=0;i<words.length;i++){
             char[] ch = words[i].toCharArray();
             if(i==0){
                 for(char c : ch)
-                    main.put(c,main.getOrDefault(c,0)+1);
+                    main[c-'a']++;
             }else{
-                Map<Character,Integer> temp = new HashMap<>();
+                int[] temp = new int[26];
                 for(char c : ch)
-                    temp.put(c,temp.getOrDefault(c,0)+1);
-                for(Character c : main.keySet()){
-                    if(temp.containsKey(c)){
-                        main.put(c,Math.min(temp.get(c),main.get(c)));
-                    }else{
-                        main.put(c,0);
-                    }
+                    temp[c-'a']++;
+                for(int j=0;j<26;j++){
+                    if(temp[j]!=0)
+                        main[j]=Math.min(temp[j],main[j]);
+                    else
+                        main[j]=0;
                 }
             }
         }
         List<String> ans = new ArrayList<>();
-        for(Character c : main.keySet()){
-            for(int i=0;i<main.get(c);i++)ans.add(c+"");
+        for(int i=0;i<26;i++){
+            for(int j=0;j<main[i];j++)ans.add((char)(i+'a')+"");
         }
         return ans;
     }
