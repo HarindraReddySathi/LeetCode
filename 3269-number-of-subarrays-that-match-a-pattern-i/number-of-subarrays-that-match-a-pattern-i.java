67441ae908c13e163e8e22301dfc55e7b26d3475
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-13 10:30:31 -0700

    Time: 12 ms (5.28%) | Memory: 44.3 MB (75.76%) - LeetSync

--- 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.java
+++ 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.java
@@ -2,33 +2,32 @@ class Solution {
     public int countMatchingSubarrays(int[] nums, int[] pattern) {
         int n = nums.length;
         int m = pattern.length;
-        int count = 0;
-        for(int i=0;i<n-m;i++){
-            boolean dec = true;
-            for(int j=0;j<m;j++){
-                if(pattern[j] == 1){
-                    if(nums[i+j+1] <= nums[i+j]) {
-                        dec = false;
-                        break;
-                    }
-                } else if(pattern[j] == 0){
-                    if(nums[i+j+1] != nums[i+j]) {
-                        dec = false;
-                        break;
-                    }
-                } else{
-                    if(nums[i+j+1] >= nums[i+j]) {
-                        dec = false;
-                        break;
-                    }
-                }
-            } 
-            if(dec) {
-                System.out.println(i);
+        int count=0;
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<n-1;i++){
+            if(nums[i+1]>nums[i]) sb.append(1);
+            else if(nums[i+1] == nums[i]) sb.append(0);
+            else sb.append(2);
+        }
+                StringBuilder sb1 = new StringBuilder();
+
+        for(int j=0;j<m;j++){
+            if(pattern[j]!=-1)sb1.append(pattern[j]);
+            else sb1.append(2);
+        }
+                        System.out.println(sb.toString());
+        int curr = 0;
+        while(curr != -1) {
+            int idx = sb.toString().indexOf(sb1.toString(), curr);
+            if(idx == -1) break;
+            else {
+                System.out.println(idx);
                 count++;
+                curr = idx+1;
             }
         }
-            return count;
+        return count;
+            //return StringUtils.countMatches(sb.toString(), sb1.toString());
 
     }
 }
