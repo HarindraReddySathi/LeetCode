b88f90e7916ddac76bf2ad22428c2bac19b6795f
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-12 19:55:27 -0700

    Time: 2 ms (19.47%) | Memory: 43.4 MB (6.03%) - LeetSync

--- 3297-minimum-time-to-revert-word-to-initial-state-i/minimum-time-to-revert-word-to-initial-state-i.java
+++ 3297-minimum-time-to-revert-word-to-initial-state-i/minimum-time-to-revert-word-to-initial-state-i.java
@@ -4,20 +4,16 @@ class Solution {
         int n = word.length();
         char[] ch = word.toCharArray();
         int p =0;
-        String temp = "";
-        for(int i=0;i<k;i++)temp+="*";
-        while(true){
-            p++;
-            word = word.substring(k,n)+temp;
-            if(compareTwoString(ch,word.toCharArray(),n)) return p;
+        for(int i =1;i<n;i++){
+            String forward = word.substring(0,i);
+            String backward = word.substring(n-i,n);
+            if(forward.equalsIgnoreCase(backward) && (n-i)%k==0){
+                p=i;
+                //System.out.println(i);
+            }
         }
-    }
-    public boolean compareTwoString(char[] ch,char[] word,int n){
-
-        for(int i=0;i<n;i++){
-            if(word[i]=='*') continue;
-            if(word[i]!=ch[i]) return false;
-        }
-        return true;
+        //System.out.println(j+"===="+k+"==="+(1d*(j+1))/k);
+        //System.out.println(Math.ceil((1d*(j+1))/k));
+        return (int)Math.ceil((1d*(n-p))/k);
     }
 }
