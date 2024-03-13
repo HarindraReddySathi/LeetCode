35eeda0106f071f84f5ea6c1162a416b90eb4996
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-12 17:45:56 -0700

    Time: 0 ms (100.00%) | Memory: 40.2 MB (94.39%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,9 +1,8 @@
 class Solution {
     public int pivotInteger(int n) {
-        long sum = n*(n+1)/2;
-        for(int i=1;i<=Math.sqrt(sum);i++){
-            if((sum+i)==i*(i+1)) return i;
-        }
+        double D = n*(n+1)/2;
+        D = (Math.sqrt(D));
+        if(D==(int)D) return (int)D;
         return -1;
     }
 }
