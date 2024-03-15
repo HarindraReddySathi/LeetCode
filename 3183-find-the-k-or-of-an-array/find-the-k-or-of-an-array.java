2d6ffabeaeb1b69df38f71eceb687f1a111f7cf3
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-15 13:42:25 -0700

    Time: 3 ms (76.92%) | Memory: 44.1 MB (25.32%) - LeetSync

---
+++ 3183-find-the-k-or-of-an-array/find-the-k-or-of-an-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findKOr(int[] nums, int k) {
+        int[] bit = new int[31];
+        for(int i : nums){
+            int pos =0;
+            while(i>0){
+                bit[pos++]+=i%2;
+                i=i/2;
+            }
+        }
+        for(int i=0;i<31;i++){
+            if(bit[i]>=k)bit[i]=1;
+            else bit[i]=0;
+        }
+        int ans =0;
+        int cur =1;
+        for(int i=0;i<31;i++){
+            if(bit[i]==1)ans+=cur;
+            cur*=2;
+        }
+        return ans;
+    }
+}
