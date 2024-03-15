68497b4eba5e7eeba86f21a039b8150d0b4c5dc5
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-15 02:27:04 -0700

    Time: 5 ms (36.36%) | Memory: 45.8 MB (6.82%) - LeetSync

---
+++ 2632-apply-bitwise-operations-to-make-strings-equal/apply-bitwise-operations-to-make-strings-equal.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean makeStringsEqual(String s, String target) {
+        
+        if(s.equals(target)) return true;
+        char[] ch1 = s.toCharArray();
+        char[] ch2 = target.toCharArray();
+        int n = s.length();
+        int up=0;
+        int down =0;
+        for(int i =0;i<n;i++){
+            if(ch1[i]=='1' && ch2[i]=='1') return true;
+            if(ch1[i]=='0' && ch2[i]=='1') up++;
+            if(ch1[i]=='1' && ch2[i]=='0') down++;
+            if(up >0 && down >0) return true;
+        }
+        return false;
+    }
+}
