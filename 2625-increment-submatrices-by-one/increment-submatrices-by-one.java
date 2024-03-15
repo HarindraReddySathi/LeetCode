76babf8af6a4e1bf6d93d960e8a8639d30afa630
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-15 00:33:47 -0700

    Time: 19 ms (76.34%) | Memory: 56.3 MB (29.03%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
@@ -7,10 +7,14 @@ class Solution {
             int j1 = queries[i][1];
             int i2 = queries[i][2];
             int j2 = queries[i][3];
-            for(int x = i1;x<=i2;x++){
-                for(int y=j1;y<=j2;y++){
-                    matrix[x][y]++;
-                }
+            for(int x =i1;x<=i2;x++){
+                matrix[x][j2]++;
+                if(j1!=0)matrix[x][j1-1]--;
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=n-2;j>=0;j--){
+                matrix[i][j]+=matrix[i][j+1];
             }
         }
         return matrix;
