53ce5094fc34f4978856628d6dcf333a921aadde
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-15 01:52:46 -0700

    Time: 69 ms (15.91%) | Memory: 60.8 MB (21.59%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.java
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.java
@@ -1,20 +1,18 @@
 class Solution {
     public long countGood(int[] nums, int k) {
-        int j=0;
-        int i=0;
-        long ans=0l;
-        long count=0l;
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        while(j<nums.length){
-            hm.put(nums[j],hm.getOrDefault(nums[j],0)+1);
-            count += hm.get(nums[j]) - 1;
-            while(count>=k){
-                ans += (nums.length - j);
-                int lf = hm.get(nums[i]);
-                count-=lf-1;
-                hm.put(nums[i], hm.get(nums[i]) - 1);
-                if(hm.get(nums[i])==0)
-                    hm.remove(nums[i]);              
+        long ans =0l;
+        int n = nums.length;
+        int i=0,j=0;
+        Map<Integer,Integer> map = new HashMap<>();
+        int pair = 0;
+        while(i<n && j<n){
+            int p = nums[j];
+            map.put(p,map.getOrDefault(p,0)+1);
+            pair+= (map.get(p)-1);
+            while(pair>=k){
+                ans+=(n-j);
+                pair-=(map.get(nums[i])-1);
+                map.put(nums[i],map.get(nums[i])-1);
                 i++;
             }
             j++;
