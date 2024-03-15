1f2b48b871ae7e70b8e7ea2bb3be41443cddec8c
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-15 03:08:43 -0700

    Time: 5 ms (95.74%) | Memory: 42.3 MB (63.57%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
@@ -1,16 +1,18 @@
 class Solution {
     public long pickGifts(int[] gifts, int k) {
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
-        for(int i : gifts)maxHeap.add(i);
+        long total =0l;
+        for(int i : gifts){
+            maxHeap.add(i);
+            total+=i;
+        }
         long ans =0;
         for(int i=0;i<k;i++){
             int p = maxHeap.poll();
             int q = (int)Math.floor(Math.sqrt(p));
+            ans+=(p-q);
             maxHeap.add(q);
         }
-        while(!maxHeap.isEmpty()){
-            ans+=maxHeap.poll();
-        }
-        return ans;
+        return total-ans;
     }
 }
