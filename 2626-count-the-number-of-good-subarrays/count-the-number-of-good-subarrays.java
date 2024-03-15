ccbb24c775687d8339f9319493e03e79a46964a2
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-15 01:38:12 -0700

    Time: 91 ms (5.11%) | Memory: 63.8 MB (5.68%) - LeetSync

---
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long countGood(int[] nums, int k) {
+        long ans =0l;
+        int n = nums.length;
+        int i=0,j=0;
+        Map<Integer,Integer> map = new HashMap<>();
+        int pair = 0;
+        while(i<n && j<n){
+            int p = nums[j];
+            map.put(p,map.getOrDefault(p,0)+1);
+            pair+= (map.get(p)-1);
+            if(pair>=k){
+                ans+=(n-j);
+                pair-=(map.get(nums[i])-1);
+                map.put(nums[i],map.get(nums[i])-1);
+                i++;
+                pair-= (map.get(p)-1);
+                map.put(p,map.get(p)-1);
+            }else{
+                j++;
+            }
+        }
+        return ans;
+    }
+}
