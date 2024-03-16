6f37ed3b83f4d4866d4d7b1549dfeff633c93dcd
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-16 09:23:39 -0700

    Time: 1409 ms (50.00%) | Memory: 70.7 MB (50.00%) - LeetSync

---
+++ 3306-mark-elements-on-array-by-performing-queries/mark-elements-on-array-by-performing-queries.java
@@ -0,0 +1,42 @@
+class Solution {
+    public long[] unmarkedSumArray(int[] nums, int[][] queries) {
+        
+        int n = nums.length;
+        int m = queries.length;
+        long[] ans = new long[m];
+        Map<Integer,Queue<Integer>> map = new TreeMap<>();
+        boolean[] isMarked = new boolean[n];
+        int marked = 0;
+        long sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            map.computeIfAbsent(nums[i],l-> new LinkedList<>()).add(i);
+        }
+        int cur=0;
+        for(int[] j : queries){
+            int pos = j[0];
+            int count = j[1];
+            if(!isMarked[pos]){
+                isMarked[pos]=true;
+                sum-=nums[pos];
+            }
+            Set<Integer> s = map.keySet();
+            for(Integer i : s){
+                while(count!=0 && map.get(i).size()!=0){
+                    Queue<Integer> q = map.get(i);
+                    int k = q.poll();
+                    if(!isMarked[k]){
+                        isMarked[k]=true;
+                        sum-=nums[k];
+                        count--;
+                    }
+                }
+                //if(map.get(i).size()==0)map.remove(i);
+                if(count==0) break;
+            }
+            ans[cur++]=sum;
+            if(sum==0) break;
+        }
+        return ans;
+    }
+}
