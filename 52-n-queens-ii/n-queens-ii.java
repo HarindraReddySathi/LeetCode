f2dcbbe4d344c1dddfdf2a32adc78fe83006d42a
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-19 14:09:34 -0700

    Time: 1 ms (87.86%) | Memory: 40.5 MB (39.90%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -4,41 +4,23 @@ class Solution {
         boolean[] down = new boolean[2*n+1];
         boolean[] x = new boolean[n];
         boolean[] y = new boolean[n];
-
-        List<List<String>> ans = new ArrayList<>();
         int[][] temp = new int[n][n];
 
-        return queens(ans,up,down,x,y,temp,0,n);
-        //return ans;
+        return queens(up,down,x,y,temp,0,n);
     }
-    public int queens(List<List<String>> ans, boolean[] up, boolean[] down, boolean[] x, boolean[] y,
-            int[][] temp, int level,int n) {
-                if(level==n){
-                    return 1;
-                }
-                int res =0;
-                for(int j=0;j<n;j++){
-                    if(!up[level+j] && !down[n-level+j] && !x[level] && !y[j] ){
-                        temp[level][j]=1;
-                        up[level+j]=true;down[n-level+j]=true;x[level]=true;y[j]=true;
-                        res+=queens(ans,up,down,x,y,temp,level+1,n);
-                        up[level+j]=false;down[n-level+j]=false;x[level]=false;y[j]=false;
-                        temp[level][j]=0;
-                    }
-                }
-                return res;
-    }
-    public void convertToString(List<List<String>> ans,int[][] memo,int n){
-
-        List<String> temp = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            StringBuilder sb = new StringBuilder();
-            for(int j=0;j<n;j++){
-                if(memo[i][j]==0)sb.append(".");
-                else sb.append("Q");
+    public int queens(boolean[] up, boolean[] down, boolean[] x, boolean[] y,
+        int[][] temp, int level,int n) {
+        if(level==n)return 1;
+        int res =0;
+        for(int j=0;j<n;j++){
+            if(!up[level+j] && !down[n-level+j] && !x[level] && !y[j] ){
+                temp[level][j]=1;
+                up[level+j]=true;down[n-level+j]=true;x[level]=true;y[j]=true;
+                res+=queens(up,down,x,y,temp,level+1,n);
+                up[level+j]=false;down[n-level+j]=false;x[level]=false;y[j]=false;
+                temp[level][j]=0;
             }
-            temp.add(sb.toString());
         }
-        ans.add(temp);
+        return res;
     }
 }
