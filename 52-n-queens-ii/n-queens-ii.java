46ceb59e241f11defbf3e5ba01ecc063d8a78ea8
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-19 14:14:19 -0700

    Time: 1 ms (87.86%) | Memory: 40.3 MB (62.95%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -1,24 +1,22 @@
 class Solution {
     public int totalNQueens(int n) {
-        boolean[] up = new boolean[2*n+1];
-        boolean[] down = new boolean[2*n+1];
+        boolean[] up = new boolean[2 * n + 1];
+        boolean[] down = new boolean[2 * n + 1];
         boolean[] y = new boolean[n];
-        int[][] temp = new int[n][n];
 
-        return queens(up,down,y,temp,0,n);
+        return queens(up, down, y, 0, n);
     }
-    public int queens(boolean[] up, boolean[] down,boolean[] y,
-        int[][] temp, int level,int n) {
-        if(level==n)return 1;
-        int res =0;
-        for(int j=0;j<n;j++){
-            if(!up[level+j] && !down[n-level+j] && !y[j] ){
-                temp[level][j]=1;
-                up[level+j]=true;down[n-level+j]=true;y[j]=true;
-                res+=queens(up,down,y,temp,level+1,n);
-                up[level+j]=false;down[n-level+j]=false;y[j]=false;
-                temp[level][j]=0;
-            }
+
+    public int queens(boolean[] up, boolean[] down, boolean[] y, int level, int n) {
+        if (level == n)
+            return 1;
+        int res = 0;
+        for (int j = 0; j < n; j++) {
+            if (up[level + j] || down[n - level + j] || y[j])
+                continue;
+            up[level + j] = down[n - level + j] = y[j] = true;
+            res += queens(up, down, y, level + 1, n);
+            up[level + j] = down[n - level + j] = y[j] = false;
         }
         return res;
     }
