c6d71597a64a9181b7e1d2db6d1e09d91b8e39c3
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-19 14:11:13 -0700

    Time: 1 ms (87.86%) | Memory: 40 MB (82.13%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -2,22 +2,21 @@ class Solution {
     public int totalNQueens(int n) {
         boolean[] up = new boolean[2*n+1];
         boolean[] down = new boolean[2*n+1];
-        boolean[] x = new boolean[n];
         boolean[] y = new boolean[n];
         int[][] temp = new int[n][n];
 
-        return queens(up,down,x,y,temp,0,n);
+        return queens(up,down,y,temp,0,n);
     }
-    public int queens(boolean[] up, boolean[] down, boolean[] x, boolean[] y,
+    public int queens(boolean[] up, boolean[] down,boolean[] y,
         int[][] temp, int level,int n) {
         if(level==n)return 1;
         int res =0;
         for(int j=0;j<n;j++){
-            if(!up[level+j] && !down[n-level+j] && !x[level] && !y[j] ){
+            if(!up[level+j] && !down[n-level+j] && !y[j] ){
                 temp[level][j]=1;
-                up[level+j]=true;down[n-level+j]=true;x[level]=true;y[j]=true;
-                res+=queens(up,down,x,y,temp,level+1,n);
-                up[level+j]=false;down[n-level+j]=false;x[level]=false;y[j]=false;
+                up[level+j]=true;down[n-level+j]=true;y[j]=true;
+                res+=queens(up,down,y,temp,level+1,n);
+                up[level+j]=false;down[n-level+j]=false;y[j]=false;
                 temp[level][j]=0;
             }
         }
