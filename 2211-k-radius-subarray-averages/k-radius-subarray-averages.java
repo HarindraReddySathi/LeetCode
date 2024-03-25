69e7b43724e0f62e18191e10cae30c3996acc75c
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-25 13:20:32 -0700

    Time: 532 ms (6.29%) | Memory: 59.8 MB (99.09%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.java
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.java
@@ -2,32 +2,18 @@ class Solution {
     public int[] getAverages(int[] nums, int k) {
         if(k==0) return nums;
         int n = nums.length;
-        //System.out.println(n);
-        if(2*k>=n){
-            nums = new int[n];
-            Arrays.fill(nums,-1);
-            return nums;
-        }
         int[] ans = new int[n];
-        long[] temp = new long[n];
-        int start =k;
-        int end = n-k;
+        Arrays.fill(ans,-1);
+        int div = (2*k+1);
+        if(2*k>=n) return ans;
         long sum=0;
         for(int i=0;i<2*k;i++){
             sum+=nums[i];
         }
-        //System.out.println(sum);
-        for(int i = start;i<end;i++){
+        for(int i = k;i<n-k;i++){
             sum+=nums[i+k];
-            temp[i]=sum;
+            ans[i]=(int)(sum/div);
             sum-=nums[i-k];
-        }
-        int div = (2*k+1);
-        for(int i=0;i<n;i++){
-            if(i<k || i>=end) ans[i]=-1;
-            else{
-                ans[i] = (int)(temp[i]/div);
-            }
         }
         return ans;
     }
