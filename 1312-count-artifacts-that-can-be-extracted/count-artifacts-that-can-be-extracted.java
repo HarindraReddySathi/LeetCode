fc7f16915a8b3cb26f2527853a3eb9d2a97f7548
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-25 02:36:02 -0700

    Time: 7 ms (45.90%) | Memory: 106.2 MB (90.16%) - LeetSync

--- 1312-count-artifacts-that-can-be-extracted/count-artifacts-that-can-be-extracted.java
+++ 1312-count-artifacts-that-can-be-extracted/count-artifacts-that-can-be-extracted.java
@@ -1,13 +1,9 @@
 class Solution {
     public int digArtifacts(int n, int[][] artifacts, int[][] dig) {
 
-        Set<String> s = new HashSet<>();
-        for(int[] i : dig){
-            StringBuilder sb = new StringBuilder();
-            sb.append(i[0]);sb.append("-");sb.append(i[1]);
-            s.add(sb.toString());
+        int[][] A = new int[n][n];
+        for(int[] d : dig) A[d[0]][d[1]] = 1;
 
-        }
         int ans =0;
         int k = artifacts.length;
         for(int i=0;i<k;i++){
@@ -15,14 +11,10 @@ class Solution {
             int c1 = artifacts[i][1];
             int r2 = artifacts[i][2];
             int c2 = artifacts[i][3];
-            //System.out.println(r1+"--"+c1+"--"+r2+"--"+c2);
             boolean dec = true;
             for(int j=r1;j<=r2;j++){
                 for(int l=c1;l<=c2;l++){
-                    //System.out.println(j+"--"+l);
-                    StringBuilder sb = new StringBuilder();
-                    sb.append(j);sb.append("-");sb.append(l);
-                    if(!s.contains(sb.toString())){
+                    if(A[j][l]==0){
                         dec = false;
                         break;
                     }
@@ -30,7 +22,6 @@ class Solution {
                 if(!dec) break;
             }
             if(dec)ans++;
-            //System.out.println(i);
         }
         return ans;
     }
