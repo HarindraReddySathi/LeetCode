d8e30f677361d0b2a7bfa3a237ef87f7d3765d76
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-24 22:14:31 -0700

    Time: 63 ms (80.00%) | Memory: 152.4 MB (60.00%) - LeetSync

--- 3376-longest-common-suffix-queries/longest-common-suffix-queries.java
+++ 3376-longest-common-suffix-queries/longest-common-suffix-queries.java
@@ -6,10 +6,6 @@ class Solution {
         Trie dummy = new Trie();
         for(int i=0;i<m;i++){
             char[] ch = wordsContainer[i].toCharArray();
-            if(dummy.min>ch.length){
-                dummy.min=ch.length;
-                dummy.pos=i;
-            }
             dummy.add(ch,0,i,ch.length);
         }
         for(int i=0;i<n;i++){
@@ -27,15 +23,10 @@ class Solution {
         Trie[] child = new Trie[26];
 
         public void add(char[] ch,int level,int i,int n){
-            if(level==ch.length) return;
+            if(n<min){min=n;pos=i;}
+            if(level==n) return ;
             int cur = (int)(ch[n-1-level]-'a');
             if(child[cur]==null)child[cur] = new Trie();
-            if(ch.length<child[cur].min){
-                //System.out.println(cur);
-                child[cur].min=ch.length;
-                child[cur].pos=i;
-            }
-            //System.out.println(n+"--"+ch[n-1-level]+"--"+child[cur].min+"---"+child[cur].pos);
             child[cur].add(ch,level+1,i,n);
         }
 
