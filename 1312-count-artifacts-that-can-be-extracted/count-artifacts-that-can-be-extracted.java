a948d52fd36cb0accca32ed424296d5c439dd0c1
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-25 02:39:57 -0700

    Time: 5 ms (72.13%) | Memory: 114.5 MB (42.62%) - LeetSync

--- 1312-count-artifacts-that-can-be-extracted/count-artifacts-that-can-be-extracted.java
+++ 1312-count-artifacts-that-can-be-extracted/count-artifacts-that-can-be-extracted.java
@@ -1,20 +1,16 @@
 class Solution {
     public int digArtifacts(int n, int[][] artifacts, int[][] dig) {
 
-        int[][] A = new int[n][n];
-        for(int[] d : dig) A[d[0]][d[1]] = 1;
+        boolean[][] A = new boolean[n][n];
+        for(int[] d : dig) A[d[0]][d[1]] = true;
 
         int ans =0;
         int k = artifacts.length;
-        for(int i=0;i<k;i++){
-            int r1 = artifacts[i][0];
-            int c1 = artifacts[i][1];
-            int r2 = artifacts[i][2];
-            int c2 = artifacts[i][3];
+        for(int[] cur : artifacts){
             boolean dec = true;
-            for(int j=r1;j<=r2;j++){
-                for(int l=c1;l<=c2;l++){
-                    if(A[j][l]==0){
+            for(int j=cur[0];j<=cur[2];j++){
+                for(int l=cur[1];l<=cur[3];l++){
+                    if(!A[j][l]){
                         dec = false;
                         break;
                     }
