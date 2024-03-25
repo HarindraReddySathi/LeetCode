0de67faeedcef3d465acb511cc72025bf78e95d4
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-25 00:23:35 -0700

    Time: 45 ms (86.42%) | Memory: 74.3 MB (39.51%) - LeetSync

--- 2240-intervals-between-identical-elements/intervals-between-identical-elements.java
+++ 2240-intervals-between-identical-elements/intervals-between-identical-elements.java
@@ -13,19 +13,14 @@ class Solution {
             List<Integer> l = map.get(i);
             for(int j=1;j<size;j++){
                 prefix[j]=prefix[j-1]+l.get(j-1);
-                //System.out.print(prefix[j]+"#####");
             }
             for(int j=size-2;j>=0;j--){
                 suffix[j]=suffix[j+1]+l.get(j+1);
-                //System.out.print(suffix[j]+"!!!!!");
             }
-            System.out.println();
             for(int j=0;j<size;j++){
                 int k = l.get(j);
-                //System.out.println(k+"----"+j+"---"+prefix[j]+"---"+suffix[j]);
                 ans[k]= (1l*j*k-prefix[j])+(suffix[j]-1l*(size-j-1)*k);
             }
-            //System.out.println("----");
         }
         return ans;
     }
