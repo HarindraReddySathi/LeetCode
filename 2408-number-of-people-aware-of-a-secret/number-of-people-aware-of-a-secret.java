6f970d04f7ea48f0dbad84375a4e6f55dd719e54
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-26 16:21:33 -0700

    Time: 13 ms (34.04%) | Memory: 44.2 MB (5.67%) - LeetSync

---
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    private static int mod = 1000000000+7;
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
+        
+        Map<Integer,Long> outspoken = new HashMap<>();
+        Map<Integer,Long> forgot = new HashMap<>();
+        long ans =1;
+        int cur=1;
+        long speaking = 0;
+        outspoken.put(cur+delay,1l);
+        forgot.put(cur+forget,1l);
+        cur++;
+        while(cur<=n){
+            if(forgot.containsKey(cur)){
+                speaking-=forgot.get(cur)%mod;
+                ans -=forgot.get(cur)%mod;
+            }
+            if(outspoken.containsKey(cur)){
+                speaking+=outspoken.get(cur)%mod;
+            }
+            if(speaking!=0){
+                outspoken.put(cur+delay,speaking%mod);
+                forgot.put(cur+forget,speaking%mod);
+            }
+            ans +=speaking%mod;
+            cur++;
+        }
+        return (int)(ans%mod);
+    }
+}
