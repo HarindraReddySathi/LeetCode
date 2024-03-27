820428a3aca120e34580fee167c3b32d6376f265
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-26 23:14:57 -0700

    Time: 12 ms (39.20%) | Memory: 45.3 MB (17.87%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
@@ -4,9 +4,6 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             m.put(nums[i],m.getOrDefault(nums[i],0)+1);
         }
-        for(int i : m.keySet()){
-            System.out.println(i+"---"+m.get(i));
-        }
         int ans =0;
         if(k==0){
             for(int i : m.keySet()){
