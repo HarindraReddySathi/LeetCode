58cdb789f52bbc94b4525bd463dd7d47a7fe574b
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-28 13:16:08 -0700

    Time: 17 ms (17.44%) | Memory: 48 MB (56.54%) - LeetSync

--- 554-brick-wall/brick-wall.java
+++ 554-brick-wall/brick-wall.java
@@ -6,15 +6,16 @@ class Solution {
         int sum =0;
         for(List<Integer> l : wall){
             sum=0;
-            for(Integer i : l){
-                sum+=i;
+            for(Integer i=0;i<l.size()-1;i++){
+                sum+=l.get(i);
                 map.put(sum,map.getOrDefault(sum,0)+1);
+                min = Math.min(min,(n-map.get(sum)));
             }
         }
-        for(Integer i : map.keySet()){
+        /*for(Integer i : map.keySet()){
             if(i==sum) continue;
             min = Math.min(min,(n-map.get(i)));
-        }
+        }*/
         return min;
     }
 }
