e1d52c80b83dbf17d950267c857cadbbffcc31b8
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-28 17:04:14 -0700

    Time: 5 ms (66.52%) | Memory: 41.7 MB (47.64%) - LeetSync

--- 553-optimal-division/optimal-division.java
+++ 553-optimal-division/optimal-division.java
@@ -3,7 +3,6 @@ class Solution {
         int n = nums.length;
         if(n==1) return String.valueOf(nums[0]);
         if(n==2) return String.valueOf(nums[0])+"/"+String.valueOf(nums[1]);
-        //return "";
         StringBuilder sb = new StringBuilder();
         sb.append(String.valueOf(nums[0]));
         sb.append("/(");
