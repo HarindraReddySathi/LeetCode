fadf1964e738922e2798a1c565e5515e461c8b08
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-03-28 17:09:03 -0700

    Time: 0 ms (100.00%) | Memory: 41.3 MB (69.53%) - LeetSync

--- 553-optimal-division/optimal-division.java
+++ 553-optimal-division/optimal-division.java
@@ -3,10 +3,12 @@ class Solution {
         int n = nums.length;
         StringBuilder sb = new StringBuilder();
         sb.append(nums[0]);
-        if(n==1) return sb.toString();
-        if(n==2) return sb.append("/").append(nums[1]).toString();
+        if (n == 1)
+            return sb.toString();
+        if (n == 2)
+            return sb.append("/").append(nums[1]).toString();
         sb.append("/(").append(nums[1]);
-        for(int i=2;i<n;i++)
+        for (int i = 2; i < n; i++)
             sb.append("/").append(String.valueOf(nums[i]));
         sb.append(")");
         return sb.toString();
