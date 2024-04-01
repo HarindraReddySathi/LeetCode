f68edeef134e087053fa72c85b750f03ee75cbd3
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-01 01:37:12 -0700

    Time: 34 ms (30.30%) | Memory: 50.3 MB (5.11%) - LeetSync

--- 3314-most-frequent-prime/most-frequent-prime.java
+++ 3314-most-frequent-prime/most-frequent-prime.java
@@ -4,44 +4,37 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
         Map<Integer, Integer> map = new HashMap<>();
-        int[] count  = new int[1];
+        int[] ans  = new int[2];
+        ans[0]=-1;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                formater(mat, i, j, 1, 0, map, primes,0,1,count);// South
-                formater(mat, i, j, 1, 1, map, primes,0,1,count);// South-East
-                formater(mat, i, j, 0, 1, map, primes,0,1,count);// East
-                formater(mat, i, j, -1, 1, map, primes,0,1,count);// North-East
-                formater(mat, i, j, -1, 0, map, primes,0,1,count);// North
-                formater(mat, i, j, -1, -1, map, primes,0,1,count);// North-West
-                formater(mat, i, j, 0, -1, map, primes,0,1,count);// West
-                formater(mat, i, j, 1, -1, map, primes,0,1,count);// South-West
+                formater(mat, i, j, 1, 0, map, primes,0,1,ans);// South
+                formater(mat, i, j, 1, 1, map, primes,0,1,ans);// South-East
+                formater(mat, i, j, 0, 1, map, primes,0,1,ans);// East
+                formater(mat, i, j, -1, 1, map, primes,0,1,ans);// North-East
+                formater(mat, i, j, -1, 0, map, primes,0,1,ans);// North
+                formater(mat, i, j, -1, -1, map, primes,0,1,ans);// North-West
+                formater(mat, i, j, 0, -1, map, primes,0,1,ans);// West
+                formater(mat, i, j, 1, -1, map, primes,0,1,ans);// South-West
             }
         }
-        int ans = -1;
-        int max = 0;
-        for (Integer i : map.keySet()) {
-            if (map.get(i) > max) {
-                ans = i;
-                max = map.get(i);
-            } else if (map.get(i) == max) {
-                if (i > ans)
-                    ans = i;
-            }
-        }
-        return ans;
+        return ans[0];
     }
 
-    public void formater(int[][] mat, int i, int j, int x, int y, Map<Integer, Integer> map, Set<Integer> primes,int cur,int fac,int[] count) {
-        //System.out.println(count[0]);
+    public void formater(int[][] mat, int i, int j, int x, int y, Map<Integer, Integer> map, Set<Integer> primes,int cur,int fac,int[] ans) {
         if(i<0 || j<0 || i>=mat.length || j>=mat[0].length) return;
-        //count[0]++;
         cur += mat[i][j]*fac;
         fac*=10;
         if(primes.contains(cur)){
             map.put(cur,map.getOrDefault(cur,0)+1);
+            if(map.get(cur)>ans[1]){
+                ans[0]=cur;
+                ans[1]=map.get(cur);
+            }else if(map.get(cur)==ans[1]){
+                if(cur>ans[0])ans[0]=cur;
+            }
         }
-        formater(mat, i+x, j+y, x, y, map, primes,cur,fac,count);
-        //count[0]--;
+        formater(mat, i+x, j+y, x, y, map, primes,cur,fac,ans);
     }
 
     public static Set<Integer> seive(){
