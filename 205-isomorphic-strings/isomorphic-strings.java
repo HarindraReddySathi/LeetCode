a267631364b9086b66ce62345416317969f2c8a9
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-01 17:41:24 -0700

    Time: 3 ms (99.09%) | Memory: 42.6 MB (26.81%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -4,16 +4,15 @@ class Solution {
         char[] ch2 = t.toCharArray();
         int[] alpha = new int[256];
         Arrays.fill(alpha ,-1);
+        Set<Integer> set = new HashSet<>();
         for(int i=0;i<ch1.length;i++){
             int pos = (int)(ch1[i]);
             int val = (int)(ch2[i]);
-            if(alpha[pos]==-1) alpha[pos]=val;
-            else if(alpha[pos]!=val) return false;
-            for(int p=0;p<256;p++){
-                if(p==pos) continue;
-                if(alpha[p]==val) return false;
-            }
-
+            if(alpha[pos]==-1){
+                if(set.contains(val)) return false;
+                set.add(val);
+                alpha[pos]=val;
+            }else if(alpha[pos]!=val) return false;
         }
         return true;
     }
