db1d8c60ef12edb5b745bf915bdf658d84ef26e9
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-10 01:30:37 -0700

    Time: 7 ms (13.92%) | Memory: 46 MB (26.58%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.java
+++ 3189-find-champion-ii/find-champion-ii.java
@@ -5,27 +5,12 @@ class Solution {
         Map<Integer,List<Integer>> map = new HashMap<>();
         for(int[] i : edges){
             map.computeIfAbsent(i[1],l-> new ArrayList<>()).add(i[0]);
-            nodes[i[0]]++;
-        }
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<n;i++){
-            if(nodes[i]==0) q.add(i);
+            nodes[i[1]]++;
         }
         int ans =-1;
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                int cur = q.poll();
-                if(!map.containsKey(cur)){
-                    if(ans!=-1) return -1;
-                    ans =cur;
-                    continue;
-                }
-                for(Integer j : map.get(cur)){
-                    nodes[j]--;
-                    if(nodes[j]==0)q.add(j);
-                }
-            }
+        for(int i=0;i<n;i++){
+            if(nodes[i]==0 && ans!=-1) return -1;
+            else if(nodes[i]==0) ans =i;
         }
         return ans;
     }
