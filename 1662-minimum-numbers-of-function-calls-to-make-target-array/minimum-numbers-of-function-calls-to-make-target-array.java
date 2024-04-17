8d628e3fa51817477a02c04d684552b2d2ac1fb5
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-17 05:29:19 -0700

    Time: 41 ms (45.76%) | Memory: 51.1 MB (76.27%) - LeetSync

---
+++ 1662-minimum-numbers-of-function-calls-to-make-target-array/minimum-numbers-of-function-calls-to-make-target-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int mul = 0;
+        int add = 0;
+        for(int i : nums){
+            int temp =0;
+            while(i>0){
+                if(i%2!=0) add++;
+                i=i/2;
+                if(i!=0) temp++;
+            }
+            mul = Math.max(mul,temp);
+        }
+        return mul+add;
+    }
+}
