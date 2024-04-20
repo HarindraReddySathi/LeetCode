cf52f12cf78b2b21e38e57fad24c7a5b58d9b00f
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-20 04:20:27 -0700

    Time: 65 ms (34.97%) | Memory: 44.3 MB (6.01%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.java
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -1,15 +1,18 @@
 class Solution {
     public int countTriples(int n) {
         int ans=0;
+        Map<Integer,Integer> numToSq = new HashMap<>();
+        Map<Integer,Integer> sqToNum = new HashMap<>();
+        for(int i =1;i<=n;i++){
+            int sq = i*i;
+            numToSq.put(i,sq);
+            sqToNum.put(sq,i);
+        }
         for(int i =1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 if(i==j) continue;
-                int k = i*i+j*j;
-                int a = (int)Math.sqrt(k);
-                if(a<=n && a*a==k){
-                    ans++;
-                    //System.out.println("( "+i+" , "+j+" , "+a+")");
-                }
+                int sq = numToSq.get(i)+numToSq.get(j);
+                if(sqToNum.containsKey(sq))ans++;
             }
         }
         return ans;
