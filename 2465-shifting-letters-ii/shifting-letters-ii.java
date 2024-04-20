c03e1fedad82240421a1ae76a91657093a06f36a
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-19 17:07:42 -0700

    Time: 7 ms (56.78%) | Memory: 74.4 MB (66.83%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.java
+++ 2465-shifting-letters-ii/shifting-letters-ii.java
@@ -2,7 +2,6 @@ class Solution {
     public String shiftingLetters(String s, int[][] shifts) {
         
         int n = s.length();
-        System.out.println(n);
         int[] shift = new int[n];
         char[] ch = s.toCharArray();
         for(int[] i : shifts){
@@ -12,9 +11,6 @@ class Solution {
         }
         for(int i=1;i<n;i++){
             shift[i] = shift[i]+shift[i-1];
-        }
-        for(int i=0;i<n;i++){
-            System.out.println(shift[i]);
         }
         for(int i=0;i<n;i++){
             int dec = shift[i]>=0 ?1 :-1;
