ae1c9830ff3e186ac70178d685f4a7e8e2fef0d0
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-21 02:34:14 -0700

    Time: 21 ms (58.84%) | Memory: 45.6 MB (51.11%) - LeetSync

--- 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.java
+++ 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.java
@@ -7,19 +7,14 @@ class Solution {
         int[] suffix = new int[n];
         if(ch[n-1]=='1')build[1]++;
         if(ch[n-1]=='0')build[0]++;
-        for(int i =n-2;i>=0;i--){
-            int dec = ch[i]=='0' ? 1:0;
-            int inc = ch[i]=='0' ? 0:1;
-            suffix[i]= build[dec];
-            build[inc]++;
-        }
         long ans =0;
         long[] memo = new long[2];
         for(int i =n-2;i>=0;i--){
             int dec = ch[i]=='0' ? 0:1;
             int inc = ch[i]=='0' ? 1:0;
+            build[dec]++;
             ans+=memo[inc];
-            memo[dec]+=suffix[i];
+            memo[dec]+=build[inc];
         }
         return ans;
     }
