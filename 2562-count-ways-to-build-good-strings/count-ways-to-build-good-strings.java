3929394bb11b5849957f244f33ed731ffbea06bf
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-21 06:38:19 -0700

    Time: 9 ms (56.07%) | Memory: 43.4 MB (65.42%) - LeetSync

---
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public int mod = 1000000000+7;
+    public int countGoodStrings(int low, int high, int zero, int one) {
+        
+        long[] memo = new long[high+1];
+        memo[zero]++;
+        memo[one]++;
+        long ans =0;
+        int start = (zero<one)?zero : one;
+        for(int i = start;i<=high;i++){
+            if(i>=low) ans = (ans+memo[i])%mod;
+            if(i+zero<=high)memo[i+zero] = (memo[i+zero]+memo[i])%mod;
+            if(i+one<=high)memo[i+one] = (memo[i+one]+memo[i])%mod;
+        }
+        return (int)ans;
+    }
+}
