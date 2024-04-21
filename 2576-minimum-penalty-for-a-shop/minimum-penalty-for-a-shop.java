c61f4ea4b181ad1738cd05a7c8a7636f67f214e7
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-21 06:07:50 -0700

    Time: 8 ms (85.93%) | Memory: 45.3 MB (33.64%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -1,33 +1,25 @@
 class Solution {
     public int bestClosingTime(String customers) {
+        int n = customers.length();
         char[] ch = customers.toCharArray();
-        int n = ch.length;
-        int[] pre = new int[n+1];
-        int[] post = new int[n+1];
-        pre[0]=0;
-        //System.out.println(pre[0]);
-        for(int i=1;i<n+1;i++){
-            int dec = (ch[i-1]=='N')?1:0;
-            pre[i]=pre[i-1]+dec;
-            //System.out.println(pre[i]);
-        }
-        post[n] =0;
-        //System.out.println(post[n]);
+
+        int res = -1;
+        int suffix = 0;
         for(int i=n-1;i>=0;i--){
-            int dec = (ch[i]=='Y')?1:0;
-            post[i]=post[i+1]+dec;
-            //System.out.println(post[i]);
+            if(ch[i] == 'Y') suffix++;
         }
-        int min = Integer.MAX_VALUE;
-        int position=-1;
-        for(int i=0;i<=n;i++){
-            int k = pre[i]+post[i];
-            //System.out.println(k);
-            if(k<min){
-                min=k;
-                position=i;
+        int prefix = 0;
+        int max = Integer.MAX_VALUE;
+        
+        for(int i=0;i<n;i++) {
+            if(suffix + prefix < max) {
+                max = suffix+prefix;
+                res = i;
             }
+            if(ch[i] == 'Y') suffix--;
+            else prefix++;
         }
-        return position;
+        if(prefix < max) return n;
+        return res;
     }
 }
