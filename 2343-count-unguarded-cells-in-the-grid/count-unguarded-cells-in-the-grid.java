453bbe54ef14e54432bd9f364c9a854f22f3f669
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-21 00:28:26 -0700

    Time: 20 ms (90.11%) | Memory: 72 MB (61.54%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
@@ -1,51 +1,47 @@
 class Solution {
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int[][] dir = {{0,1},{0,-1},{-1,0},{1,0}};
-        boolean[][][] isVisited = new boolean[m][n][4];
-        Queue<int[]> q = new LinkedList<>();
-
-        for(int[] i : guards) {
-            isVisited[i[0]][i[1]][0] = true;
-            isVisited[i[0]][i[1]][1] = true;
-            isVisited[i[0]][i[1]][2] = true;
-            isVisited[i[0]][i[1]][3] = true;
-            int[] p0 = {i[0],i[1],0};
-            q.add(p0);
-            int[] p1 = {i[0],i[1],1};
-            q.add(p1);
-            int[] p2 = {i[0],i[1],2};
-            q.add(p2);
-            int[] p3 = {i[0],i[1],3};
-            q.add(p3);
+        int[][] res = new int[m][n];   //We create the matrix with the proper dimension
+        int result = 0, cnti, cntj;
+        for(int[] i : walls){       //We insert all the walls
+            res[i[0]][i[1]] = 2;
         }
-
-        for(int[] i : walls) {
-            isVisited[i[0]][i[1]][0] = true;
-            isVisited[i[0]][i[1]][1] = true;
-            isVisited[i[0]][i[1]][2] = true;
-            isVisited[i[0]][i[1]][3] = true;
+        for(int[] i : guards){      //We insert all the guards
+            res[i[0]][i[1]] = 1;  
         }
-
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                int[] cur = q.poll();
-                int[] move = dir[cur[2]];
-                int x = cur[0]+move[0];
-                int y = cur[1]+move[1];
-                if(x<0 || x>m-1 || y<0 || y> n-1) continue;
-                if(isVisited[x][y][cur[2]]) continue;
-                isVisited[x][y][cur[2]] = true;
-                int[] push = {x,y,cur[2]};
-                q.add(push);
+        for(int i = 0; i < res.length;i++){
+            for(int j = 0; j < res[i].length;j++){
+                if(res[i][j] == 1){     //If we found a guard...
+                    cnti = i;   //Position of the guard
+                    cntj = j;   //Position of the guard
+                    while(cnti-1 != -1 && res[cnti-1][cntj] != 2 && res[cnti-1][cntj] != 1){  //If we can go up in the matrix...
+                        res[cnti-1][cntj] = 3;
+                        cnti--;
+                    }
+                    cnti = i;  //We reset the value to the initial one
+                    while(cnti+1 != m && res[cnti+1][cntj] != 2 && res[cnti+1][cntj] != 1){   //If we can go down in the matrix...
+                        res[cnti+1][cntj] = 3;
+                        cnti++;
+                    }
+                    cnti = i;  //We reset the value to the initial one
+                    while(cntj-1 != -1 && res[cnti][cntj-1] != 2 && res[cnti][cntj-1] != 1){  //If we can go to left in the matrix...
+                        res[cnti][cntj-1] = 3;
+                        cntj--;
+                    }
+                    cntj = j;  //We reset the value to the initial one
+                    while(cntj+1 != n && res[cnti][cntj+1] != 2 && res[cnti][cntj+1] != 1){  //If we can go to rigth in the matrix...
+                        res[cnti][cntj+1] = 3;
+                        cntj++;
+                    }
+                }
             }
         }
-        int ans =0;
-        for(int i =0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(!(isVisited[i][j][0] || isVisited[i][j][1] ||isVisited[i][j][2] || isVisited[i][j][3] )) ans++;
+        for(int[] i : res){   //Once we have marked the correct squares in the matrix...
+            for(int j : i){
+                if(j == 0){   //If we find a '0', we add one to the counter as it is an unguarded cell
+                    result++;
+                }
             }
         }
-        return ans;
+        return result;  //We return the number of unguarded cells
     }
 }
