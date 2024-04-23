94e1e7525cbe13a693e408bea18901f5b6f383e0
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-23 15:51:01 -0700

    Time: 22 ms (75.23%) | Memory: 56.3 MB (57.34%) - LeetSync

---
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> ans = new ArrayList<>();
+        if(n==1){
+            ans.add(0);
+            return ans;
+        } 
+        Map<Integer,List<Integer>> map = new HashMap<>();
+        int[] isVisited = new int[n];
+        for(int[] i : edges){
+            map.computeIfAbsent(i[0],l->new ArrayList<>()).add(i[1]);
+            map.computeIfAbsent(i[1],l->new ArrayList<>()).add(i[0]);
+        }
+        for(int[] i : edges){
+            isVisited[i[0]]++;
+            isVisited[i[1]]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        int count = n;
+        for(int i=0;i<n;i++){
+            //System.out.println(i+" -- "+isVisited[i]);
+            if(isVisited[i]==1){
+                q.add(i);
+                isVisited[i]--;
+                count--;
+                //System.out.println(i);
+            }
+        }
+        while(!q.isEmpty()){
+            if(count==0) break;
+            int size = q.size();
+            for(int j =0;j<size;j++){
+                int cur = q.poll();
+                for(int i : map.get(cur)){
+                    if(isVisited[i]==0) continue;
+                    isVisited[i]--;
+                    if(isVisited[i]==1){
+                        q.add(i);
+                        isVisited[i]--;
+                        count--;
+                    }
+                }
+            }
+        }
+        while(!q.isEmpty()){
+            ans.add(q.poll());
+        }
+        
+        return ans;
+    }
+}
