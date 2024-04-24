0c6d6ec148c2117c0b6f9c31b4595e5b9d068d17
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-23 19:28:26 -0700

    Time: 5 ms (99.11%) | Memory: 52 MB (57.52%) - LeetSync

--- 1359-circular-permutation-in-binary-representation/circular-permutation-in-binary-representation.java
+++ 1359-circular-permutation-in-binary-representation/circular-permutation-in-binary-representation.java
@@ -1,8 +1,8 @@
 class Solution {
     public List<Integer> circularPermutation(int n, int start) {
         List<Integer> ans = new ArrayList<>();
-        int end = (int)Math.pow(2,n)-1;
-        for(int i=0;i<=end;i++){
+        int end = 1<<n;
+        for(int i=0;i<end;i++){
             ans.add((i^(i>>1))^start);
         }
         return ans;
