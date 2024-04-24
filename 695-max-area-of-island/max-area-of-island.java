5131e361cadacd7e4080f9446c491a1c1c822c92
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-24 14:28:47 -0700

    Time: 2 ms (68.66%) | Memory: 44.1 MB (85.03%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -2,12 +2,11 @@ class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        boolean[][] check = new boolean[m][n];
         int max = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                    int temp = DFS(grid,check,i,j,m,n);
+                    int temp = DFS(grid,i,j,m,n);
                     if(temp>max)max = temp;
                 }
             }
@@ -15,15 +14,14 @@ class Solution {
         return max;
     }
 
-    public int DFS(int[][] grid,boolean[][] check,int i,int j, int m, int n){
+    public int DFS(int[][] grid,int i,int j, int m, int n){
         if(i<0 || j<0 || i>=m || j>=n || grid[i][j]==-1 || grid[i][j]==0) return 0;
         grid[i][j]=-1;
-        //check[i][j]=true;
         int count=1;
-        count+=DFS(grid,check,i+1,j,m,n);
-        count+=DFS(grid,check,i-1,j,m,n);
-        count+=DFS(grid,check,i,j+1,m,n);
-        count+=DFS(grid,check,i,j-1,m,n);
+        count+=DFS(grid,i+1,j,m,n);
+        count+=DFS(grid,i-1,j,m,n);
+        count+=DFS(grid,i,j+1,m,n);
+        count+=DFS(grid,i,j-1,m,n);
         return count;
     }
 }
