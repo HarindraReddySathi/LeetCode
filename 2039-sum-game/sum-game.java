1755e4a6381a4dc1071ef9189bed8a757e5517d0
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-24 12:47:52 -0700

    Time: 17 ms (6.12%) | Memory: 45.9 MB (6.12%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int[] sum = new int[2];
+        int[] q = new int[2];
+        char[] ch = num.toCharArray();
+        for(int i =0;i<n;i++){
+            int pos = (i<n/2) ?0:1;
+            if(ch[i]=='?')q[pos]++;
+            else{
+                sum[pos]+=Integer.parseInt(String.valueOf(ch[i]));
+            }
+        }
+        if((sum[0]>sum[1] && q[0]>q[1])||(sum[1]>sum[0] && q[1]>q[0])) return true;
+        int diff = (sum[0]>sum[1]) ? sum[0]-sum[1] : sum[1]-sum[0];
+        int vaca = (q[0]>q[1]) ? q[0]-q[1] : q[1]-q[0];
+        if(vaca%2!=0 || diff%9!=0) return true;
+        if(vaca/2==diff/9) return false;
+        return true;
+    }
+}
