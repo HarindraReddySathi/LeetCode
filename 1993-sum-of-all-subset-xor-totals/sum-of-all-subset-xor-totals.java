af05184471d45c9fa715c1f9c2188671ef687d20
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-23 23:51:00 -0700

    Time: 5 ms (39.00%) | Memory: 40.9 MB (38.54%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,31 +1,14 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
         
-        int n = nums.length;
-        int stage =0;
-        int cur=1;
-        int ans =0;
-        while(stage<5){
-            int[] digit = new int[2]; 
-            for(int i=0;i<n;i++){
-                digit[nums[i]&1]++;
-                nums[i] = nums[i]>>1;
+        int sum = 0;
+        for (int i= 1 ;i<1<<nums.length;++i){
+            int temp = 0 ; 
+            for(int k=0;k<nums.length;++k){
+                if((i&(1<<k))!=0)temp^=nums[k]; 
             }
-            int zero = (int)Math.pow(2,digit[0]);
-            int one =0;
-            int up = digit[1];
-            int down = 1;
-            int numer = digit[1];
-            for(int j=1;j<=digit[1];j=j+2){
-                one+=(numer);
-                numer = (numer*(up-1)*(up-2))/((down+1)*(down+2));
-                up-=2;
-                down+=2;
-            }
-            ans+=(one*zero*cur);
-            stage++;
-            cur*=2;
+            sum+=temp;
         }
-        return ans;
+        return sum;
     }
 }
