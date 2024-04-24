1be2ae09eca507b4cc87fadd9f3176e39d7da5e3
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-23 23:50:03 -0700

    Time: 0 ms (100.00%) | Memory: 40.4 MB (91.90%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -11,22 +11,17 @@ class Solution {
                 digit[nums[i]&1]++;
                 nums[i] = nums[i]>>1;
             }
-            //System.out.println(stage + " -- "+digit[0]+" -- "+ digit[1]);
             int zero = (int)Math.pow(2,digit[0]);
             int one =0;
             int up = digit[1];
             int down = 1;
             int numer = digit[1];
-            int denom = 1;
             for(int j=1;j<=digit[1];j=j+2){
-                //System.out.println(numer + "--" + denom);
-                one+=(numer/denom);
-                numer = numer*(up-1)*(up-2);
-                denom = denom*(down+1)*(down+2);
+                one+=(numer);
+                numer = (numer*(up-1)*(up-2))/((down+1)*(down+2));
                 up-=2;
                 down+=2;
             }
-            //System.out.println(one*zero*cur);
             ans+=(one*zero*cur);
             stage++;
             cur*=2;
