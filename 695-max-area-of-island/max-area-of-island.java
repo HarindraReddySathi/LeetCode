971a506cd128817cd81166318127de7cba6bf32a
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-24 14:32:15 -0700

    Time: 1 ms (100.00%) | Memory: 44.8 MB (27.47%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -16,13 +16,7 @@ class Solution {
 
     public int DFS(int[][] grid,int i,int j, int m, int n){
         if(i<0 || j<0 || i>=m || j>=n || grid[i][j]<1) return 0;
-        grid[i][j]=-1;
-        /*int count=1;
-        count+=DFS(grid,i+1,j,m,n);
-        count+=DFS(grid,i-1,j,m,n);
-        count+=DFS(grid,i,j+1,m,n);
-        count+=DFS(grid,i,j-1,m,n);
-        return count;*/
+        grid[i][j]=0;
         return 1+DFS(grid,i+1,j,m,n)+DFS(grid,i-1,j,m,n)+DFS(grid,i,j+1,m,n)+DFS(grid,i,j-1,m,n);
     }
 }
