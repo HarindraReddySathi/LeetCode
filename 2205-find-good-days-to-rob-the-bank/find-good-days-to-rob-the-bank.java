880997ba74d4a80337e382aa163c3b615e2ccc0e
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-25 00:54:04 -0700

    Time: 6 ms (77.59%) | Memory: 59.4 MB (24.07%) - LeetSync

--- 2205-find-good-days-to-rob-the-bank/find-good-days-to-rob-the-bank.java
+++ 2205-find-good-days-to-rob-the-bank/find-good-days-to-rob-the-bank.java
@@ -6,27 +6,25 @@ class Solution {
             for(int i=0;i<n;i++) ans.add(i);
             return ans;
         }
-        int[][] memo = new int[n][2];
+        int[] memo = new int[n];
         int cur =0;
-        for(int i=1;i<n;i++){
-            if(nums[i]<=nums[i-1]){
-                cur++;
-            }else{
-                cur=0;
-            }
-            if(cur>=time)memo[i][0]=1;
-        }
-        cur=0;
         for(int i=n-2;i>=0;i--){
             if(nums[i]<=nums[i+1]){
                 cur++;
             }else{
                 cur=0;
             }
-            if(cur>=time)memo[i][1]=1;
+            if(cur>=time)memo[i]++;
         }
-        for(int i=0;i<n;i++){
-            if(memo[i][0]==1 && memo[i][1]==1) ans.add(i);
+        cur=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]<=nums[i-1]){
+                cur++;
+            }else{
+                cur=0;
+            }
+            if(cur>=time)memo[i]++;
+            if(memo[i]==2)ans.add(i);
         }
         return ans;
     }
