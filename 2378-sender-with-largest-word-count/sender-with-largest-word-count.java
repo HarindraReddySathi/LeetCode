f59ef78bd11aad71d3ab7057cf8bcbeb4293eafe
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-26 00:29:58 -0700

    Time: 100 ms (19.37%) | Memory: 51.1 MB (80.18%) - LeetSync

--- 2378-sender-with-largest-word-count/sender-with-largest-word-count.java
+++ 2378-sender-with-largest-word-count/sender-with-largest-word-count.java
@@ -1,7 +1,7 @@
 class Solution {
     public String largestWordCount(String[] messages, String[] senders) {
         
-        Map<String,Integer> map = new HashMap<>();
+        Map<String,Integer> map = new TreeMap<>(Collections.reverseOrder());
         int n = messages.length;
         int max = -1;
         for(int i =0;i<n;i++){
@@ -9,16 +9,12 @@ class Solution {
             for(char c : messages[i].toCharArray()){
                 if(c==' ')wordCount++;
             }
-            //int wordCount = messages[i].split(" ").length;
             map.put(senders[i],map.getOrDefault(senders[i],0)+wordCount);
             if(map.get(senders[i])>max)max = map.get(senders[i]);
         }
-        String ans = "";
         for(String s : map.keySet()){
-            if(map.get(s)==max){
-                if(s.compareTo(ans)>0) ans =s;
-            }
+            if(map.get(s)==max) return s;
         }
-        return ans;
+        return "";
     }
 }
