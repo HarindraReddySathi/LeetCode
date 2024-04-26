873c9e54afa949dbccf08c0eda0d1dd07aa0f85e
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-26 06:24:13 -0700

    Time: 4 ms (87.82%) | Memory: 62.1 MB (21.15%) - LeetSync

--- 2604-minimum-operations-to-make-array-equal-ii/minimum-operations-to-make-array-equal-ii.java
+++ 2604-minimum-operations-to-make-array-equal-ii/minimum-operations-to-make-array-equal-ii.java
@@ -2,12 +2,18 @@ class Solution {
     public long minOperations(int[] nums1, int[] nums2, int k) {
         
         int n = nums1.length;
+        if(k==0){
+            for(int i=0;i<n;i++){
+                if(nums1[i]!=nums2[i]) return -1;
+            }
+            return 0;
+        }
         long pos =0;
         long neg =0;
         for(int i=0;i<n;i++){
             if(nums1[i]==nums2[i]) continue;
-            if(k==0 || (nums1[i]-nums2[i])%k!=0) return -1;
-            long steps = (1l*(nums1[i]-nums2[i]))/k;
+            if((nums1[i]-nums2[i])%k!=0) return -1;
+            int steps = ((nums1[i]-nums2[i]))/k;
             if(steps>0)pos+=steps;
             else neg+=steps;
         }
