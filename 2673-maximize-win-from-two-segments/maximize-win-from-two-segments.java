78b0056071db8124b78e5000382c25712b76bcc9
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-26 02:47:02 -0700

    Time: 6 ms (39.44%) | Memory: 57.7 MB (19.72%) - LeetSync

--- 2673-maximize-win-from-two-segments/maximize-win-from-two-segments.java
+++ 2673-maximize-win-from-two-segments/maximize-win-from-two-segments.java
@@ -2,19 +2,16 @@ class Solution {
     public int maximizeWin(int[] p, int k) {
         
         int n = p.length;
-        Map<Integer,Integer> prefix = new HashMap<>();
-        Map<Integer,Integer> suffix = new HashMap<>();
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
         int i=0,j=0,max =-1;
         int ans =0;
         while(j<n){
 
             while(j<n && p[j]-p[i]<=k){
-                if(!prefix.containsKey(p[j]) || prefix.get(p[j])<j-i+1){
-                    if(max<j-i+1)max = j-i+1;
-                    prefix.put(p[j],max);
-                    if(ans<j-i+1) ans = max;
-                }
-                //System.out.println(j-i+1);
+                if(max<j-i+1)max = j-i+1;
+                prefix[j]=max;
+                if(ans<j-i+1) ans = max;
                 j++;
             }
             i++;
@@ -24,12 +21,9 @@ class Solution {
         while(j>=0){
 
             while(j>=0 && p[i]-p[j]<=k){
-                if(!suffix.containsKey(p[j]) || suffix.get(p[j])<i-j+1){
-                    if(max<i-j+1)max = i-j+1;
-                    suffix.put(p[j],max);
-                    if(ans<i-j+1) ans = max;
-                }
-                //System.out.println(i-j+1);
+                if(max<i-j+1)max = i-j+1;
+                suffix[j]=max;
+                if(ans<i-j+1) ans = max;
                 j--;
             }
             i--;
@@ -37,7 +31,7 @@ class Solution {
         i=0;
         while(i<n-1){
             if(p[i]!=p[i+1]){
-                int cur = prefix.get(p[i])+suffix.get(p[i+1]);
+                int cur = prefix[i]+suffix[i+1];
                 if(cur>ans) ans =cur;
             }
             i++;
