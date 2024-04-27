67d99bd0f2c1a16716972f2fc3fca644226fbf6b
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-27 01:18:03 -0700

    Time: 5 ms (94.51%) | Memory: 45 MB (85.65%) - LeetSync

--- 1530-check-if-a-string-can-break-another-string/check-if-a-string-can-break-another-string.java
+++ 1530-check-if-a-string-can-break-another-string/check-if-a-string-can-break-another-string.java
@@ -4,22 +4,22 @@ class Solution {
         int n = s1.length();
         char[] ch1 = s1.toCharArray();
         char[] ch2 = s2.toCharArray();
-        Arrays.sort(ch1);
-        Arrays.sort(ch2);
-        int dec =0;
+        int[] a1 = new int[26];
+        int[] a2 = new int[26];
         for(int i=0;i<n;i++){
-            if(ch1[i]==ch2[i]) continue;
-            else if(ch1[i]<ch2[i]){
-                if(dec ==0){
-                    dec =1;
-                }
-                if(dec==-1) return false;
-            }else{
-                if(dec ==0){
-                    dec =-1;
-                }
-                if(dec==1) return false;
-            }
+            a1[ch1[i]-'a']++;a2[ch2[i]-'a']++;
+        }
+        boolean ans = true;
+        int dec = 0;
+        int sum1 =0;
+        int sum2= 0;
+        for(int i=0;i<26;i++){
+            sum1 += a1[i];
+            sum2 += a2[i];
+            if(sum1==sum2) continue;
+            if((sum1-sum2)*dec<0) return false;
+            if(sum1>sum2) dec =1;
+            else dec =-1;
         }
         return true;
     }
