e0bb39cd49f3af0476aebd4e77f62de6e9285b73
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-27 01:45:35 -0700

    Time: 1 ms (97.96%) | Memory: 40.6 MB (42.86%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.java
@@ -3,32 +3,23 @@ class Solution {
         
         char[] ch = Integer.toString(num).toCharArray();
         int n = ch.length;
-        //high
+        //high - Low
         char high = 'p';
-        for(char c : ch){
-            if(c!='9'){
-                high=c;
-                break;
-            }
-        }
-        //Low
         char low = 'p';
         int val = 1;
         if(ch[0]!='1'){
-            //System.out.println("****");
             low = ch[0];
         }
-        //System.out.println(high + " --- "+low);
-        if(low=='p'){
-            val=0;
-            for(char c : ch){
-                if(c!='0' && c!='1'){
-                    low=c;
-                    break;
-                }
+        if(low=='p'){val=0;}
+        for(char c : ch){
+            if(high == 'p' && c!='9'){
+                high=c;
             }
+            if(low=='p' && c!='0' && c!='1'){
+                 low=c;
+            }
+            if(high!='p' && low!='p') break;
         }
-        //System.out.println(high + " --- "+low);
         /// Calculation
         int ans =0;
         int cur =1;
