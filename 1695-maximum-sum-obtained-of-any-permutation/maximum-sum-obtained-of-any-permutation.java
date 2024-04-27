aee911a18cd23ce7150edc4bc8ecddcfc857ae9e
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-27 02:24:52 -0700

    Time: 26 ms (45.21%) | Memory: 79.6 MB (35.62%) - LeetSync

--- 1695-maximum-sum-obtained-of-any-permutation/maximum-sum-obtained-of-any-permutation.java
+++ 1695-maximum-sum-obtained-of-any-permutation/maximum-sum-obtained-of-any-permutation.java
@@ -9,18 +9,14 @@ class Solution {
             arr[i[0]]++;
             if(i[1]+1<n) arr[i[1]+1]--;
         }
-        PriorityQueue<Integer> maxHeap = new PriorityQueue(Collections.reverseOrder());
-        maxHeap.add(arr[0]);
-        //System.out.println(arr[0]);
         for(int i = 1;i<n;i++){
             arr[i]+=arr[i-1];
-            //System.out.println(arr[i]);
-            maxHeap.add(arr[i]);
         }
+        Arrays.sort(arr);
         Arrays.sort(nums);
         long ans =0;
         for(int i=n-1;i>=0;i--){
-            int p1 = maxHeap.poll();
+            int p1 = arr[i];
             ans = (ans+((1l*p1*nums[i])%mod))%mod;
         }
         return (int)ans;
