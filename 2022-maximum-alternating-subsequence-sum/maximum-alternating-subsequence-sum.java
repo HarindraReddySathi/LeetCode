510a7aba15e08a7c184a13c7416ac9f98124c7d8
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-28 04:34:59 -0700

    Time: 5 ms (89.87%) | Memory: 58.9 MB (95.47%) - LeetSync

---
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long maxAlternatingSum(int[] nums) {
+        
+        int n = nums.length;
+        if(n==1) return nums[0];
+        long ans =0;
+        int prev =0;
+        for(int i=0;i<n;i++){
+            if(prev ==0){
+                prev = nums[i];
+            }else{
+                if(nums[i]>prev){
+                    prev = nums[i];
+                    continue;
+                }
+                if(i!=n-1){
+                    if(nums[i]<nums[i+1]){
+                        ans+=(prev-nums[i]);
+                        prev=0;
+                    }
+                }
+            }
+        }
+        return ans+prev;
+
+    }
+}
