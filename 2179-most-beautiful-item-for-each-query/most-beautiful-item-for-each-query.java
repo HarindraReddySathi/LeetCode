8b8b6f88b7609cb91e0f7b6692f523d084c66689
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-04-28 06:59:23 -0700

    Time: 43 ms (82.48%) | Memory: 81.4 MB (40.15%) - LeetSync

---
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] maximumBeauty(int[][] items, int[] queries) {
+        
+        int n = items.length;
+        Arrays.sort(items,(a,b) -> a[0]-b[0]);
+        int[] beauty = new int[n];
+        beauty[0]=items[0][1];
+        for(int i =1;i<n;i++){
+            if(items[i][1]>beauty[i-1]){
+                beauty[i]=items[i][1];
+            }else{
+                beauty[i] = beauty[i-1];
+            }
+        }
+        int[] res = new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            int low =0;
+            int high = n-1;
+            int ans =0;
+            while(low<=high){
+                int mid = low+(high-low)/2;
+                if(items[mid][0]<=queries[i]){
+                    ans = beauty[mid];
+                    low = mid+1;
+                }else{
+                    high = mid -1;
+                }
+            }
+            res[i]=ans;
+        }
+        return res;
+    }
+}
