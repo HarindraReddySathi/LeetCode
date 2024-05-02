6bdf8978421251857e7260d6a813921a93034b41
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-05-01 20:41:56 -0700

    Time: 41 ms (14.17%) | Memory: 16.5 MB (46.76%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -1,12 +1,21 @@
+# class Solution:
+#     def reversePrefix(self, word: str, ch: str) -> str:
+#         # val=0
+#         for k in range(len(word)):
+#             if word[k]==ch:
+#                 # val=k
+#                 return word[k::-1]+word[k+1:]
+#         return word
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        # val=0
-        for k in range(len(word)):
-            if word[k]==ch:
-                # val=k
-                return word[k::-1]+word[k+1:]
+        x=0
+        for i in range(len(word)):
+            if word[x]==ch:
+                return word[x::-1]+word[x+1:]
+            x+=1
         return word
-        
+
+              
         
 
         
