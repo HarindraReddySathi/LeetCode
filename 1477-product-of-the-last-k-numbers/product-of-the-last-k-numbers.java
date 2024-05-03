da1042f102d64aec8603a2da8fd7a884302388bd
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-05-02 19:59:09 -0700

    Time: 14 ms (99.89%) | Memory: 68.9 MB (23.23%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
@@ -1,35 +1,27 @@
 class ProductOfNumbers {
-
-    List<Integer> list;
-    int prev;
+    List<Integer> prods;
+    int prod; 
     public ProductOfNumbers() {
-        list = new ArrayList();
-        list.add(1);
-        prev = 1;
+       prods = new ArrayList<>();
+       prods.add(1);
+       prod = 1;
     }
 
     public void add(int num) {
-        //if element is > 0 create next element in a prefix product list. Update prev to be this
-        //element
-        if (num > 0) {
-            prev*=num;
-            list.add(prev);
+        if(num != 0) {
+            prods.add(num*prod);
+            prod*=num;
         }
-        //if this is 0  we need to reinit the structure
         else {
-            list = new ArrayList();
-            list.add(1);
-            prev = 1;
+            prods = new ArrayList<>();
+            prods.add(1);
+            prod = 1;
         }
     }
 
     public int getProduct(int k) {
-        int N = list.size();
-        //in case there are not enough elements by the problem definition it can only happen if
-        //we have met 0 before. In this case return 0. In all other cases we get the product from
-        //division due to prefix product property. Note that list always has n + 1 elements due to 
-        //initial 1, we need it to avoid outofbounds checks for edge cases
-        return (k < N) ? prev / list.get(N - 1 - k) : 0;
+        int n = prods.size();
+        return k<n ? prod/prods.get(n-k-1) : 0;
     }
 }
 
