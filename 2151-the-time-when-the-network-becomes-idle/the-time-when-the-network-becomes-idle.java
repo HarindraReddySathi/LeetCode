37dbe0817ca5bf288c58f660ac723ec5e3b76242
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-05-03 12:30:25 -0700

    Time: 131 ms (25.53%) | Memory: 98.1 MB (86.17%) - LeetSync

--- 2151-the-time-when-the-network-becomes-idle/the-time-when-the-network-becomes-idle.java
+++ 2151-the-time-when-the-network-becomes-idle/the-time-when-the-network-becomes-idle.java
@@ -13,13 +13,15 @@ class Solution {
         int ans = Integer.MIN_VALUE;
         while(!q.isEmpty()){
             int size = q.size();
+            int p = (level<<1);
             for(int i =0;i<size;i++){
                 int cur = q.poll();
                 for(int j : map.get(cur)){
                     if(patience[j]>0){
                         //System.out.println(level + " --- " + patience[j]);
-                        int temp = (((level<<1)/patience[j])*patience[j])+(level<<1);
-                        if((level<<1)%patience[j]==0)temp = ((((level<<1)/patience[j])-1)*patience[j])+(level<<1);
+                        int temp = 0;
+                        if((p)%patience[j]==0)temp = ((((p)/patience[j])-1)*patience[j])+(p);
+                        else temp = (((p)/patience[j])*patience[j])+(p);
                         //System.out.println(level + " --- ");
                         if(ans<temp) ans =temp;
                         patience[j]=0;
