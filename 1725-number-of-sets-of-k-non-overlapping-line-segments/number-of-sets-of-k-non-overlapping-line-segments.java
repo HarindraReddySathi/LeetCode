84e246efc6a1f278a4b8de745516a4f6f2452392
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-05-04 01:10:25 -0700

    Time: 17 ms (91.30%) | Memory: 46.4 MB (91.30%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -2,17 +2,17 @@ class Solution {
     public int mod = 1000000007;
     public int numberOfSets(int n, int k) {
         
-        long[][] memo = new long[k+1][n];
-        long ans =0,cur =1;
+        int[][] memo = new int[k+1][n];
+        int cur =1;
         for(int j=n-2;j>=0;j--,cur++){
             memo[1][j] = (memo[1][j+1]+cur)%mod;
         }
         for(int i =2;i<k+1;i++){
-            long prev =0;
+            int prev =0;
             for(int j = n-i-1;j>=0;j--){
                 //if(n-j<=i) continue;
-                memo[i][j] = (prev+memo[i-1][j+1]+memo[i][j+1])%mod;
                 prev = (prev+memo[i-1][j+1])%mod;
+                memo[i][j] = (prev+memo[i][j+1])%mod;
             }
         }
         return (int)(memo[k][0]%mod);
