41ee598a004da8be1f440ff35a351a06f683d8c5
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-05-06 04:41:32 -0700

    Time: 7 ms (80.63%) | Memory: 69.4 MB (33.07%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -10,9 +10,34 @@
  */
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        if (head == null) return null;
-        head.next = removeNodes(head.next);
-        return head.next != null && head.val < head.next.val ? head.next : head;
-        
+        head = reverse( head);
+        ListNode root = new ListNode(0);
+        root.next = head;
+        ListNode prev = root;
+        int high = 0;
+        while(head!=null){
+            if(head.val>=high){
+                prev=prev.next;
+                if(head.val>high) high = head.val;
+            }else{
+                prev.next = head.next;
+            }
+            head = head.next;
+        }
+        return reverse( root.next);
+    }
+
+    public ListNode reverse(ListNode head){
+
+        ListNode temp = null;
+        ListNode next = null;
+        while(head.next!=null){
+            next = head.next;
+            head.next = temp;
+            temp = head;
+            head = next;
+        }
+        head.next = temp;
+        return head;
     }
 }
