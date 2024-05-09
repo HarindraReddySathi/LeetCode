d8bb979107349dee417f0d84eb17c8a119fdf330
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-05-09 03:43:34 -0700

    Time: 10 ms (66.93%) | Memory: 44.7 MB (36.72%) - LeetSync

--- 1534-minimum-number-of-frogs-croaking/minimum-number-of-frogs-croaking.java
+++ 1534-minimum-number-of-frogs-croaking/minimum-number-of-frogs-croaking.java
@@ -3,13 +3,11 @@ class Solution {
         
         int n = croakOfFrogs.length();
         char[] ch = croakOfFrogs.toCharArray();
-        Map<Character,Integer> map = new HashMap<>();
-        map.put('c',0);map.put('r',1);map.put('o',2);map.put('a',3);map.put('k',4);
         int[] ap = new int[5];
         int ans =0;
         int pend =0;
         for(char c : ch){
-            int cur = map.get(c);
+            int cur = dec(c);
             if(cur==0){
                 ap[cur]++;
                 if(pend==0) ans++;
@@ -34,5 +32,20 @@ class Solution {
             if(ap[i]!=0) return -1;
         }
         return ans;
+    }
+
+     public int dec(char a) {
+        
+        if(a=='c')
+        return 0;
+        if(a=='r')
+        return 1;
+        if(a=='o')
+        return 2;
+        if(a=='a')
+        return 3;
+        if(a=='k')
+        return 4;
+        return -1;
     }
 }
