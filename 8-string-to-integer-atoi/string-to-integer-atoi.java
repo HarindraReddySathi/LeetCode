1bc3d4085d55d3ee3186a95e820502d0d1d29dd8
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-05-12 02:52:44 -0700

    Time: 6 ms (9.42%) | Memory: 42.5 MB (32.05%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,31 +1,54 @@
 class Solution {
     public int myAtoi(String s) {
-        
-        StringBuilder sb = new StringBuilder();
+        /*
+            if String is valid -
+                1. we need nod consider leading zeros and white spaces
+                2. if we find '-' at start it is a negative number
+                3. what if the number exceeds the limit of an integer - return the extemes
+            if String is invalid - 
+                1. may be contains anything other than numbers,'.','-' throw some error 
+                2. if '-' is not present at start then it is invalid
+                3. Dont consider the number after '.' since we are storing only integer 
+        */
+        if("".equals(s)) return 0;
         char[] ch = s.toCharArray();
         int n = ch.length;
-        int i = removeWhiteSpaces(ch,n);
-        if(i==n) return 0;
-        if(ch[i]!='+' && ch[i]!='-' && ch[i]-'0'<0 && ch[i]-'9'>0) return 0;
-        if(ch[i]=='-') sb.append(ch[i++]);
-        else if(ch[i]=='+') sb.append(ch[i++]);
-        while(i<n&&(ch[i]-'0'>=0&&ch[i]-'0'<10)){
-            sb.append(ch[i++]);
-        }
-        if(sb.length()==0 ||(sb.length()==1 && (sb.charAt(0)=='+' || sb.charAt(0)=='-'))) return 0;
-        System.out.println(sb.toString());
-        double ans = Double.parseDouble(sb.toString());
-        if(ans<=Integer.MIN_VALUE) return Integer.MIN_VALUE;
-        if(ans>=Integer.MAX_VALUE) return Integer.MAX_VALUE;
-        return (int)ans;
+        s = s.trim();
+        if("".equals(s)) return 0;
+        int sign = s.charAt(0)=='-' ? -1 : 1;
+        s = makeValid(s);
+        if("".equals(s)) return 0;
+        if(s.length()>10) return (sign ==1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        return valueGenerator(s,sign);
     }
 
-    public int removeWhiteSpaces(char[] ch ,int n){
-
-        for(int i=0;i<n;i++){
-            if(ch[i]!=' ') return i;
+    public int valueGenerator(String s , int sign){
+        System.out.println(sign);
+        int n = s.length();
+        char[] ch = s.toCharArray();
+        long value = 0l;
+        long cur_place_val = 1;
+        for(int i =n-1;i>=0;i--,cur_place_val*=10){
+            value += (1l*(int)(ch[i]-'0')*cur_place_val*sign);
+            //System.out.println(value);
+            if(value > Integer.MAX_VALUE) return (sign==1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            if(value < Integer.MIN_VALUE) return (sign==1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
         }
-        return n;
+        return (int)value;
     }
 
+    public String makeValid(String s){
+        int n = s.length();
+        char[] ch = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        int start = (ch[0]=='-' || ch[0]=='+') ? 1 : 0;
+        boolean isLeadingZero = true;
+        for(int i =start;i<n;i++){
+            if((ch[i]>'9' || ch[i]<'0')) break;
+            if(ch[i]=='0' && isLeadingZero) continue;
+            sb.append(ch[i]);
+            if(isLeadingZero) isLeadingZero = false;
+        }
+        return sb.toString();
+    }
 }
