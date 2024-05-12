20c18f445754c9bd9760f679ff87dee0b2cba5a5
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-05-12 08:16:35 -0700

    Time: 1 ms (100.00%) | Memory: 44.9 MB (5.19%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -15,39 +15,29 @@ class Solution {
         ListNode first_prev = null;
         ListNode second_prev = null;
         int carry =0;
-        while(l1!=null && l2!=null){
-            int sum = l1.val+l2.val+carry;
+        while(l1!=null || l2!=null || carry!=0){
+            int x = l1!=null ? l1.val : 0;
+            int y = l2!=null ? l2.val : 0;
+            int sum = x+y+carry;
             carry = sum/10;
             sum = sum%10;
-            l1.val = sum;
-            l2.val = sum;
+            if(l1==null && l2==null){
+                if(first_prev!=null){
+                    first_prev.next = new ListNode(1);
+                    return first_list;
+                }
+                if(second_prev!=null){
+                    second_prev.next = new ListNode(1);
+                    return second_list;
+                }
+            }
+            if(l1!=null) l1.val = sum;
+            if(l2!=null) l2.val = sum;
             first_prev = l1;
             second_prev = l2;
-            l1 = l1.next;
-            l2 = l2.next;
+            if(l1!=null) l1 = l1.next;
+            if(l2!=null) l2 = l2.next;
         }
-
-        while(l1!=null){
-            int sum = l1.val+carry;
-            carry = sum/10;
-            sum = sum%10;
-            l1.val = sum;
-            first_prev = l1;
-            l1 = l1.next;
-        }
-        if(l2==null){
-            if(carry!=0) first_prev.next = new ListNode(carry);
-            return first_list;
-        }
-        while(l2!=null){
-            int sum = l2.val+carry;
-            carry = sum/10;
-            sum = sum%10;
-            l2.val = sum;
-            second_prev = l2;
-            l2 = l2.next;
-        }
-        if(carry!=0) second_prev.next = new ListNode(carry);
-        return second_list;
+        return first_prev!=null ? first_list : second_list;
     }
 }
