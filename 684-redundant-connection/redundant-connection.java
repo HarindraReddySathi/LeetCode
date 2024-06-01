8cc671373f0f0120a7f5de249167411129445fb9
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-06-01 13:21:34 -0700

    Time: 1 ms (74.78%) | Memory: 43 MB (66.96%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        int[] parent = new int[n+1];
+        for(int i=1;i<n+1;i++)parent[i]=i;
+        int[] size = new int[n+1];
+        Arrays.fill(size,1);
+        int[] result = new int[2];
+        for(int[] edge : edges){
+            int left = edge[0],right = edge[1];
+            int leftSize =0,rightSize=0;
+            while(parent[left]!=left){
+                left = parent[left];
+            }
+            leftSize = size[left];
+            while(parent[right]!=right){
+                right = parent[right];
+            }
+            rightSize = size[right];
+            if(left==right){
+                result = edge;
+                continue;
+            }
+            if(leftSize>=rightSize){
+                parent[right]= left;
+                size[left]+=size[right];
+            }else{
+                parent[left]= right;
+                size[right]+=size[left];
+            }
+        }
+        return result;
+
+    }
+}
