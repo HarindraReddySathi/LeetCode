ae3ee49bdd91dd469da0d45c51f9aa5dd02b00b5
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-06-01 15:40:38 -0700

    Time: 10 ms (5.33%) | Memory: 42.3 MB (5.05%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.java
+++ 365-water-and-jug-problem/water-and-jug-problem.java
@@ -22,7 +22,8 @@ class Solution {
                 visited.add(x+"_"+current[1]);
             }
             //case 3 pour water from second to first
-                int one = (current[0]+current[1]>x) ? x: current[0]+current[1];
+            if(transfer(visited,queue,target,current[0],current[1],x,false)) return true;
+                /*int one = (current[0]+current[1]>x) ? x: current[0]+current[1];
                 int second = (current[1]-(one-current[0])>0) ? current[1]-one+current[0] : 0;
                 if(one==target || second==target || one+second==target) return true;
                 if(!visited.contains(one+"_"+second)){
@@ -36,10 +37,11 @@ class Solution {
                 if(!visited.contains(0+"_"+second)){
                     queue.add(new int[]{0,second});
                     visited.add(0+"_"+second);
-                }
+                }*/
 
             //case 4 pour water from first to second
-                second = (current[0]+current[1]>y) ? y: current[0]+current[1];
+            if(transfer(visited,queue,target,current[1],current[0],y,true)) return true;
+                /*second = (current[0]+current[1]>y) ? y: current[0]+current[1];
                 one = (current[0]-(second-current[1])>0) ? current[0]-second+current[1] : 0;
                 if(one==target || second==target || one+second==target) return true;
                 if(!visited.contains(one+"_"+second)){
@@ -53,9 +55,34 @@ class Solution {
                 if(!visited.contains(0+"_"+second)){
                     queue.add(new int[]{0,second});
                     visited.add(0+"_"+second);
-                }
+                }*/
         }
 
         return false;
+    }
+
+    public boolean transfer(Set<String> visited, Queue<int[]> queue,int target,int a,int b ,int aLimit,boolean dec){
+
+                int one = (a+b>aLimit) ? aLimit: a+b;
+                int second = (b-one+a>0) ? b-one+a : 0;
+                if(dec) {
+                    int temp = one;
+                    one = second;
+                    second = one;
+                }
+                if(one==target || second==target || one+second==target) return true;
+                if(!visited.contains(one+"_"+second)){
+                    queue.add(new int[]{one,second});
+                    visited.add(one+"_"+second);
+                }
+                if(!visited.contains(one+"_"+0)){
+                    queue.add(new int[]{one,0});
+                    visited.add(one+"_"+0);
+                }
+                if(!visited.contains(0+"_"+second)){
+                    queue.add(new int[]{0,second});
+                    visited.add(0+"_"+second);
+                }
+                return false;
     }
 }
