393778b509379208fe6135c29c543893561484c1
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-06-01 16:38:33 -0700

    Time: 67 ms (8.90%) | Memory: 47.1 MB (18.32%) - LeetSync

---
+++ 1757-minimum-jumps-to-reach-home/minimum-jumps-to-reach-home.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int minimumJumps(int[] forbidden, int a, int b, int x) {
+
+        Set<String> forbiddenSteps = new HashSet<>();
+        int maxforbiddenStep =0;
+        for(int forbiddenStep : forbidden){
+            forbiddenSteps.add(forbiddenStep+"_"+0);
+            forbiddenSteps.add(forbiddenStep+"_"+1);
+            if(forbiddenStep>maxforbiddenStep)maxforbiddenStep=forbiddenStep;
+        }
+        //int limit = findLimit(a,b,x);
+        //int limit = Math.max(maxforbiddenStep+ a + b, x + b);
+        int limit =6000;
+        if(forbiddenSteps.contains(0) || forbiddenSteps.contains(x)) return -1;
+
+        Queue<int[]> queue = new LinkedList<>();
+        //{position,numberOfBackSteps}
+        queue.add(new int[]{0,0});
+        forbiddenSteps.add(0+"_"+0);
+        int minSteps =0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0;i<size;i++){
+                int[] current = queue.poll();
+                if(current[0]==x) return minSteps;
+                if(current[0]-b <= limit && !forbiddenSteps.contains((current[0]+a)+"_"+0)){
+                    queue.add(new int[]{current[0]+a,0});
+                    forbiddenSteps.add((current[0]+a)+"_"+0);
+                }
+
+                if(current[1]!=1){
+                    if(current[0]-b>=0 && !forbiddenSteps.contains((current[0]-b)+"_"+1)){
+                        queue.add(new int[]{current[0]-b,1});
+                        forbiddenSteps.add((current[0]-b)+"_"+1);
+                    }
+                }
+            }
+            minSteps++;
+        }
+        return -1;
+    }
+
+    public int findLimit(int forward , int backward,int target){
+
+        if(forward>= 2*backward) return target+forward;
+        return target+3000*backward;
+    }
+}
