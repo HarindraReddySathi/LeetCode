37268043b17e86da9896cd464ca8fa79938fde96
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-06-03 17:24:15 -0700

    Time: 3 ms (74.70%) | Memory: 41.6 MB (71.92%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -3,23 +3,29 @@ class Solution {
         
         int[][] alpha = new int[26][2];
         char[] ch = s.toCharArray();
+        int ans =0;
+        int oddCount=0;
         for(char c : ch){
             if(c>='a' && c<='z'){
                 alpha[c-'a'][0]++;
+                if(alpha[c-'a'][0]==2){
+                    ans+=2;
+                    alpha[c-'a'][0]=0;
+                    oddCount--;
+                }else{
+                    oddCount++;
+                }
             }else{
                 alpha[c-'A'][1]++;
+                if(alpha[c-'A'][1]==2){
+                    ans+=2;
+                    alpha[c-'A'][1]=0;
+                    oddCount--;
+                }else{
+                    oddCount++;
+                }
             }
         }
-        int ans = 0;
-        boolean odd = true;
-        for(int[] i : alpha){
-            //System.out.println(i[0]+" -- "+i[1]);
-            ans+=2*((i[0]/2)+(i[1]/2));
-            if(odd && (i[0]%2!=0 || i[1]%2!=0)){
-                ans++;
-                odd = false;
-            }
-        }
-        return ans;
+        return (oddCount>0) ? ans+1 : ans;
     }
 }
