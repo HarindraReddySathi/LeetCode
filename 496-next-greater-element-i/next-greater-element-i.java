ce5ee0606e7ff72970dd37793f3f7f846a18cde1
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-06-09 20:51:49 -0700

    Time: 3 ms (91.04%) | Memory: 44.1 MB (26.56%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,33 +1,28 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<n2;i++){
-            map.put(nums2[i],i);
+        int count = nums2.length;
+        Stack<Integer> monotonicStack = new Stack<>();
+        Map<Integer,Integer> numberToNextGreaterElement = new HashMap<>();
+        
+        for(int i = count-1; i>=0; i--){
+
+            int cur = nums2[i];
+
+            while(!monotonicStack.isEmpty() && nums2[i]>=monotonicStack.peek()){
+                monotonicStack.pop();
+            }
+
+            nums2[i] = (monotonicStack.isEmpty()) ?  -1 : monotonicStack.peek();
+
+            numberToNextGreaterElement.put(cur,nums2[i]);
+
+            monotonicStack.push(cur);
         }
-        nextElementGreater(nums2,n2);
-        for(int i=0;i<n1;i++){
-            nums1[i]= nums2[map.get(nums1[i])];
+
+        for(int i =0;i<nums1.length;i++){
+            nums1[i] = numberToNextGreaterElement.get(nums1[i]);
         }
         return nums1;
-    }
-
-    public void nextElementGreater(int[] nums,int n){
-        Stack<Integer> s = new Stack<>();
-        s.push(-1);
-        for(int i=n-1;i>=0;i--){
-            int p = nums[i];
-            while(!s.isEmpty() && nums[i]>s.peek()){
-                s.pop();
-            }
-            if(s.isEmpty()) nums[i]=-1;
-            else{
-                nums[i]=s.peek();
-            }
-            s.push(p);
-            //System.out.println( nums[i]);
-        }
     }
 }
