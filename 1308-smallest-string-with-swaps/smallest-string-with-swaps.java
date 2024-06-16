486dcaa69b5be1d9b254f0b6888c94b7595967c8
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-06-16 13:27:25 -0700

    Time: 42 ms (59.61%) | Memory: 95.7 MB (69.33%) - LeetSync

--- 1308-smallest-string-with-swaps/smallest-string-with-swaps.java
+++ 1308-smallest-string-with-swaps/smallest-string-with-swaps.java
@@ -2,45 +2,44 @@ class Solution {
     public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
         
         int n = s.length();
-        int[] parent = new  int[n];
-        for(int i=0;i<n;i++) parent[i]=i;
+        char[] ch = s.toCharArray();
+        int[] parent = new int[n];
         int[] size = new int[n];
         Arrays.fill(size,1);
+        for(int i=0;i<n;i++) parent[i]=i;
 
-        for (List<Integer> pair : pairs) {
-            int left = find(parent, pair.get(0));
-            int right = find(parent, pair.get(1));
+        for(List<Integer> pair : pairs){
+            
+            int left = findParent(pair.get(0),parent);
+            int right = findParent(pair.get(1),parent);
 
-            if (left != right) {
-                if (size[left] >= size[right]) {
-                    parent[right] = left;
-                    size[left] += size[right];
-                } else {
-                    parent[left] = right;
-                    size[right] += size[left];
-                }
+            if(size[left]>=size[right]){
+                parent[right] = left;
+                size[left]+=size[right];
+            }else{
+                parent[left] = right;
+                size[right]+=size[left];
             }
+
         }
 
-        Map<Integer, PriorityQueue<Character>> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            int root = find(parent, i);
-            map.computeIfAbsent(root, k -> new PriorityQueue<>()).add(s.charAt(i));
-        }
+        Map<Integer,PriorityQueue<Character>> map = new HashMap<>();
 
+        for(int i=0;i<n;i++){
+            int root = findParent(i,parent);
+            map.computeIfAbsent(root, pq -> new PriorityQueue<>()).add(ch[i]);
+        }
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            int root = find(parent, i);
-            sb.append(map.get(root).poll());
+        for(int i=0;i<n;i++){
+            sb.append(map.get(parent[i]).poll()+"");
         }
-
         return sb.toString();
     }
 
-    private int find(int[] parent, int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent, parent[x]);
+    public int findParent(int i ,int[] parent){
+        if(parent[i]!=i){
+            parent[i] = findParent(parent[i],parent);
         }
-        return parent[x];
+        return parent[i];
     }
 }
