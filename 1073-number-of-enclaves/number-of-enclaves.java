23680e4362468ae869b537fdeea9dd1423e9d7ec
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-06-16 13:01:22 -0700

    Time: 8 ms (76.16%) | Memory: 49.3 MB (99.16%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -8,8 +8,12 @@ class Solution {
         for(int i =0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                    ans++;
-                    if(i==0 || i == m-1 || j==0 || j==n-1)queue.add(new int[]{i,j});
+                    if(i==0 || i == m-1 || j==0 || j==n-1){
+                        queue.add(new int[]{i,j});
+                        grid[i][j]=0;
+                    }else{
+                        ans++;
+                    }
                 }
             }
         }
@@ -18,14 +22,13 @@ class Solution {
             int[] current = queue.poll();
             int curX = current[0];
             int curY = current[1];
-            if(grid[curX][curY]==0) continue;
-            ans--;
-            grid[curX][curY]=0;
             for(int[] direction : directions){
                 int x = curX+direction[0];
                 int y = curY+direction[1];
                 if(x<0 || y<0 || x>=m || y>=n || grid[x][y]==0) continue;
                 queue.add(new int[]{x,y});
+                grid[x][y]=0;
+                ans--;
             }
         }
         return ans;
