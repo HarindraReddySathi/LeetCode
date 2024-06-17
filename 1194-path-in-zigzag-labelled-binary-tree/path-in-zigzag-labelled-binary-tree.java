d2dc637ede5d7a2c9c347502e4aea7636eb314f9
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-06-17 11:56:37 -0700

    Time: 0 ms (100.00%) | Memory: 40.5 MB (96.76%) - LeetSync

---
+++ 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Integer> pathInZigZagTree(int label) {
+        
+        List<Integer> zigZagLabelledBT = new ArrayList<>();
+        int pow = 0;
+        // 1 -> even -1 -> odd
+        int isEven = -1;
+        while(pow<label){
+            pow = (pow+1)*2-1;
+            isEven*=-1;
+        }
+
+        while(pow >= 1){
+            zigZagLabelledBT.add(label);
+            int start  = (pow+1)/2;
+            int offset = (label-start)/2;
+            label = start -1 - offset;
+            pow = start -1;
+            isEven*=-1;
+        }
+
+        Collections.reverse(zigZagLabelledBT);
+        return zigZagLabelledBT;
+    }
+}
