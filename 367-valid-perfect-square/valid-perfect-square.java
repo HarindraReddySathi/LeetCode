c34152768908a2035880fc639512af3880b7f6bd
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-06-17 13:23:02 -0700

    Time: 0 ms (100.00%) | Memory: 40.8 MB (8.90%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long low =1 ;
+        long high = num;
+        while(low<=high){
+            long mid = low+(high-low)/2;
+            long cur = mid*mid;
+            if(cur == num) return true;
+            else if(cur>num) high = mid-1;
+            else low = mid+1;
+        }
+        return false;
+    }
+}
