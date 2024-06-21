08d519a0cb4ecd2b0923074392d8121fb02c4294
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-06-21 03:07:29 -0700

    Time: 111 ms (17.84%) | Memory: 56.9 MB (51.88%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -1,36 +1,39 @@
 class Solution {
     public int minimizedMaximum(int n, int[] quantities) {
-        
-        int m =  quantities.length;
-
-        //int low = Integer.MAX_VALUE;
+        int m = quantities.length;
         int low = 1;
         int high = Integer.MIN_VALUE;
-
-        for(int i=0;i<m;i++){
-            //if(quantities[i]<low) low = quantities[i];
-            if(quantities[i]>high) high = quantities[i];
-        }
-        int ans = -1;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-
-            if(isPossible(mid,quantities,n)){
-                ans = mid;
-                high = mid-1;
-            }else{
-                low = mid+1;
+        
+        // Find the maximum quantity in the array
+        for (int i = 0; i < m; i++) {
+            if (quantities[i] > high) {
+                high = quantities[i];
             }
         }
-
+        
+        int ans = -1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (isPossible(mid, quantities, n)) {
+                ans = mid; // mid is a possible answer, try to find a smaller one
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
         return ans;
     }
-
-    public boolean isPossible(int mid,int[] quantities,int n){
-
-        for(int i : quantities){
-            n -= Math.ceil((double)i/mid);
+    
+    public boolean isPossible(int mid, int[] quantities, int n) {
+        int requiredItems = 0;
+        
+        for (int quantity : quantities) {
+            requiredItems += Math.ceil((double) quantity / mid); // ceil(i / mid) is the number of items needed for quantity i
         }
-        return (n>=0);
+        
+        return requiredItems <= n;
     }
 }
