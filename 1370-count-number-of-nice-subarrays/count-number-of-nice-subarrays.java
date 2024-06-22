d78103aa192f9088962bc3316d0b71466d80ce9b
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-06-22 02:34:15 -0700

    Time: 25 ms (20.57%) | Memory: 55 MB (36.41%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,28 +1,26 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        return subArraysAtMostK(nums, k) - subArraysAtMostK(nums, k - 1);
+        
+        int a =  subArraysGreaterThanK(nums,k);
+        int b = subArraysGreaterThanK(nums,k+1);
+        System.out.println(a +" -- " + b);
+        return a-b;
     }
 
-    public int subArraysAtMostK(int[] nums, int k) {
+    public int subArraysGreaterThanK(int[] nums , int k){
+
         int n = nums.length;
-        int count = 0;
-        int ans = 0;
-        int j = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] % 2 != 0) {
-                count++;
+        int count =0;
+        int ans =0;
+        for(int i=0,j=0 ;i<n;i++){
+            if(i!=0){
+                if(nums[i-1]%2!=0) count--;
             }
-
-            while (count > k) {
-                if (nums[j++] % 2 != 0) {
-                    count--;
-                }
+            while(j<n && count<k){
+                if(nums[j++]%2!=0) count++;
             }
-
-            ans += i - j + 1;
+            if(count>=k)ans += n-j+1; 
         }
-
         return ans;
     }
 }
