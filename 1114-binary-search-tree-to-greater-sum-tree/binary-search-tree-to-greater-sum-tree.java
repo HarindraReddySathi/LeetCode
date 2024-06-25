c29c70727035a31e14038f85d0db3c9304730fb7
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-06-25 16:49:00 -0700

    Time: 0 ms (100.00%) | Memory: 41.1 MB (31.98%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
@@ -15,19 +15,17 @@
  */
 class Solution {
     public TreeNode bstToGst(TreeNode root) {
-        
-        int[] sum = new int[1];
-        traversal(root,sum);
+        greaterSumTreeBuilder(root,0);
         return root;
     }
 
-    public void traversal(TreeNode root, int[] sum){
+    public int greaterSumTreeBuilder(TreeNode root,int prevSum){
 
-        if(root == null) return;
+        if(root == null) return prevSum;
 
-        traversal(root.right,sum);
-        sum[0]+=root.val;
-        root.val = sum[0];
-        traversal(root.left,sum);
+        int sum = greaterSumTreeBuilder(root.right,prevSum);
+        root.val+=sum;
+        return greaterSumTreeBuilder(root.left,root.val);
+
     }
 }
