2c314e262830ee0a04b62852d65b39664b23ddbb
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-06-27 22:48:11 -0700

    Time: 7 ms (87.57%) | Memory: 62.6 MB (76.76%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.java
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.java
@@ -10,15 +10,6 @@ class Solution {
         for(int i=n-1,cur=n;i>=0;i--,cur--){
             ans = ans+ (1l*cur*nodes[i]);
         }
-        /*PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i : nodes){
-            maxHeap.add(i);
-        }
-        long ans =0;
-        int cur = n;
-        while(!maxHeap.isEmpty()){
-            ans = ans + (1l*maxHeap.poll()*cur--);
-        }*/
         return ans;
     }
 }
