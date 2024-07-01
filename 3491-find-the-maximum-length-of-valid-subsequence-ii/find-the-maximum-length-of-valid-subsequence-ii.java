8b0a79cb95ca8bd44a2f58cd7b5e09158437c190
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-07-01 04:52:27 -0400

    Time: 33 ms (100.00%) | Memory: 45 MB (33.33%) - LeetSync

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.java
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.java
@@ -1,30 +1,21 @@
 class Solution {
     public int maximumLength(int[] nums, int k) {
+        
         int n = nums.length;
-
-        // Ensure all elements are positive and within the range [0, k-1]
-        for (int i = 0; i < n; i++) {
-            nums[i] = ((nums[i] % k) + k) % k;
+        for(int i=0;i<n;i++){
+            nums[i] %=k;
         }
-
         int maximumLength = 0;
-
-        // Iterate over all possible remainders
-        for (int j = 0; j < k; j++) {
+        for(int j=0;j<k;j++){
             int[] status = new int[k];
-            int length = 0;
-
-            for (int i = 0; i < n; i++) {
-                int remainder = nums[i];
-                int req = (k + j - remainder) % k;
-
-                status[remainder] = Math.max(status[remainder], status[req] + 1);
-                length = Math.max(length, status[remainder]);
+            int length=0;
+            for(int i=0;i<n;i++){
+                int req = (k+j-nums[i])%k;
+                status[nums[i]] = Math.max(status[nums[i]],status[req]+1);
+                length = Math.max(status[nums[i]],length);
             }
-
-            maximumLength = Math.max(maximumLength, length);
+            maximumLength = Math.max(length,maximumLength);
         }
-
         return maximumLength;
     }
 }
