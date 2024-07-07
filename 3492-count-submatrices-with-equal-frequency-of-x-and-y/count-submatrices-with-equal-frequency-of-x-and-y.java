cfe77e588e7fe520b8ca9113f96d01f5db62783c
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-07-07 02:10:29 -0400

    Time: 99 ms (50.00%) | Memory: 162.1 MB (50.00%) - LeetSync

---
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int numberOfSubmatrices(char[][] grid) {
+        
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int subMatricesCount = 0;
+        int[][][] state = new int[m][n][2];
+
+        for(int i =0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='X') state[i][j][0]++;
+                else if(grid[i][j]=='Y') state[i][j][1]++;
+                if(j!=0){
+                    state[i][j][0] += state[i][j-1][0];
+                    state[i][j][1] += state[i][j-1][1];
+                }
+            }
+            //if(i==0) System.out.println("( "+state[i][n-1][0]+" , "+state[i][n-1][1]+" ), ");
+            for(int j=0;j<n;j++){
+                if(i!=0){
+                    state[i][j][0] += state[i-1][j][0];
+                    state[i][j][1] += state[i-1][j][1];
+                }
+                if(state[i][j][0]!=0 && state[i][j][0] == state[i][j][1]){
+                    subMatricesCount++;
+                    //System.out.println(i +" -- "+j);
+                }
+            }
+        }
+
+        /*for(int i =0;i<m;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("( "+state[i][j][0]+" , "+state[i][j][1]+" ), ");
+            }
+            System.out.println();
+        }*/
+        return subMatricesCount;
+    }
+}
