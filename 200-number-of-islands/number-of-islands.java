a68bc74f6322c67c22890bef67fdad6340b35329
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-08-21 01:05:05 -0400

    Time: 5 ms (31.47%) | Memory: 52.5 MB (7.11%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,26 +1,40 @@
 class Solution {
     public int numIslands(char[][] grid) {
         
-        int m=grid.length;
-        int n=grid[0].length;
-        int ans =0;
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int numOfIslands =0;
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                    ans++;
-                    DFS(grid,i,j,m,n);
+                    numOfIslands++;
+                    BFS(i, j, grid,m,n);
                 }
             }
         }
-        return ans;
+
+        return numOfIslands;
     }
 
-    public void DFS(char[][] grid,int i,int j,int m,int n){
-        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]=='0') return;
+    public void BFS(int i,int j,char[][] grid,int m,int n){
+
+        int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
+        Queue<int[]> traverse = new LinkedList<>();
+        traverse.add(new int[]{i,j});
         grid[i][j]='0';
-        DFS(grid,i-1,j,m,n);
-        DFS(grid,i+1,j,m,n);
-        DFS(grid,i,j-1,m,n);
-        DFS(grid,i,j+1,m,n);
+
+        while(!traverse.isEmpty()){
+            int[] current = traverse.poll();
+            for(int[] direction : directions){
+                int x = current[0]+direction[0];
+                int y = current[1]+direction[1];
+                if(x>=0 && y>=0 && x<m && y<n && grid[x][y]=='1'){
+                    traverse.add(new int[]{x,y});
+                    grid[x][y]='0';
+                }
+            }
+        }
     }
 }
