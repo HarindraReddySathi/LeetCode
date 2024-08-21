aa676554ae25e8c6b1c467b7fafeb525e1d41995
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-08-21 17:29:55 -0400

    Time: 0 ms (100.00%) | Memory: 40.7 MB (74.38%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,32 +1,21 @@
 class Solution {
-
-    //Iterative
     public int rob(int[] nums) {
+        
         int n = nums.length;
+
         if(n==1) return nums[0];
-        int[] memo = new int[n+1];
-        memo[1]=nums[0];
-        for(int i=2;i<=n;i++){
-            memo[i]=Math.max(memo[i-1],memo[i-2]+nums[i-1]);
+
+        int pre = nums[1]>=nums[0] ? nums[1] : nums[0];
+        int prePre = nums[0];
+
+        for(int i=2;i<n;i++){
+            int temp = pre;
+
+            pre = Math.max(pre,prePre+nums[i]);
+            prePre = temp;
         }
-        return memo[n];
+
+        return (pre>=prePre) ? pre : prePre;
+
     }
-
-
-    /*
-    //Recurssive
-    public int rob(int[] nums) {
-        int n = nums.length;
-        int[] memo = new int[n+1];
-        Arrays.fill(memo,-1);
-        return itr(nums,n,memo);
-    }
-
-    public int itr(int[] nums,int n,int[] memo){
-        if(n==1) return nums[0];
-        if(n==0) return 0;
-        if(memo[n]!=-1) return memo[n];
-        memo[n]= Math.max(itr(nums,n-1,memo),itr(nums,n-2,memo)+nums[n-1]);
-        return memo[n];
-    }*/
 }
