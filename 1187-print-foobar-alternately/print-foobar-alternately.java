75ac41bae3043fe0878e37a66c1dd3f208fb7498
HarindraReddySathi <115383352+HarindraReddySathi@users.noreply.github.com>
2024-09-01 16:07:05 -0400

    Time: 22 ms (94.23%) | Memory: 43.3 MB (81.20%) - LeetSync

--- 1187-print-foobar-alternately/print-foobar-alternately.java
+++ 1187-print-foobar-alternately/print-foobar-alternately.java
@@ -1,44 +1,42 @@
 class FooBar {
 
-    private ReentrantLock lock ;
-    private Condition alter;
     private boolean isFoo;
     private int n;
 
     public FooBar(int n) {
         this.n = n;
         this.isFoo = false;
-        this.lock = new ReentrantLock();
-        this.alter = lock.newCondition();
     }
 
     public void foo(Runnable printFoo) throws InterruptedException {       
         
         for (int i = 0; i < n; i++) {
-                lock.lock();
-                while(isFoo){
-                    alter.await();
+
+            synchronized(this){
+                 while(isFoo){
+                    wait();
                 }
         	    // printFoo.run() outputs "foo". Do not change or remove this line.
         	    printFoo.run();
                 isFoo = true;
-                alter.signal();
-                lock.unlock();
+                notify();
+            }
+               
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
         
         for (int i = 0; i < n; i++) {
-                lock.lock();
+             synchronized(this){
                 while(!isFoo){
-                    alter.await();
+                    wait();
                 }
         	    // printBar.run() outputs "bar". Do not change or remove this line.
         	    printBar.run();
                 isFoo = false;
-                alter.signal();
-                lock.unlock();
+                notify();
+             }
         }
     }
 }
